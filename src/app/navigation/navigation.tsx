076ea7b1708f9a2f3a0da01df7d237b6b06adfc7
Jimmy Emery <jemery@allcampus.com>
2019-10-22 22:18:18 -0400

    updating navigation

--- src/app/navigation/navigation.tsx
+++ src/app/navigation/navigation.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
+import { animated, useSpring } from 'react-spring';
 import * as styles from './navigation.module.scss';
 
-const Navigation: React.FC = () => (
-  <nav className={styles.nav}>
-    This is the nav
-  </nav>
-);
+const Navigation: React.FC = () => {
+  const [isExpanded, setState] = React.useState(false);
+  const styleProps = useSpring({ width: isExpanded ? '20rem' : '10rem' });
+  return (
+    <animated.nav
+      className={styles.nav}
+      style={styleProps}
+    >
+      This is the nav
+      {isExpanded ? 'EXPANDED' : 'COLLAPSED'}
+      <button onClick={() => setState(!isExpanded)} type="button">Click Me</button>
+      <div>foo bar</div>
+    </animated.nav>
+  );
+};
 
 export default Navigation;
