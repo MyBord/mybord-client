860ccc09a0ecaf37715903e2b20ac495148db7f1
Jimmy Emery <jemery@allcampus.com>
2019-11-03 15:42:02 -0600

    updating hamburger button test

--- src/app/shared/buttons/hamburgerButton/hamburgerButton.test.tsx
+++ src/app/shared/buttons/hamburgerButton/hamburgerButton.test.tsx
@@ -1,23 +1,33 @@
-// import * as React from 'react';
-// import { ShallowWrapper, shallow } from 'enzyme';
-// import LottiePlayer from 'lotty/lottiePlayer';
-// import HamburgerButton from './hamburgerButton';
-//
-// const setup = (): ShallowWrapper => shallow(
-//   <HamburgerButton onClick={() => {}} />,
-// );
-//
-// describe('basic rendering', () => {
-//   let wrapper: ShallowWrapper;
-//   beforeEach(() => {
-//     wrapper = setup();
-//   });
-//
-//   test('component renders', () => {
-//     expect(wrapper.length).toBe(1);
-//   });
-//
-//   test('Lottie component renders', () => {
-//     expect(wrapper.find(LottiePlayer).length).toBe(1);
-//   });
-// });
+import * as React from 'react';
+import Lottie from 'react-lottie';
+import { ReactWrapper, mount } from 'enzyme';
+import LottiePlayer from 'lotty/lottiePlayer';
+import HamburgerButton from './hamburgerButton';
+
+const setup = (): ReactWrapper => mount(
+  <HamburgerButton
+    direction={2}
+    isStopped={true}
+    onClick={() => {}}
+  />,
+);
+
+describe('basic rendering', () => {
+  let wrapper: ReactWrapper;
+  beforeEach(() => {
+    wrapper = setup();
+  });
+
+  test('component renders', () => {
+    expect(wrapper.length).toBe(1);
+  });
+
+  test('Lottie component renders', () => {
+    expect(wrapper.find(LottiePlayer).length).toBe(1);
+  });
+
+  test('`direction` and `isStopped` are properly drilled', () => {
+    expect(wrapper.find(LottiePlayer).find(Lottie).prop('direction')).toBe(2);
+    expect(wrapper.find(LottiePlayer).find(Lottie).prop('isStopped')).toBeTruthy();
+  })
+});
