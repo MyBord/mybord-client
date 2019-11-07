be0c1b80c92ab8a153c9c162bce4a4ee48918a27
Jimmy Emery <jemery@allcampus.com>
2019-11-07 18:53:27 -0500

    adding transitions

--- src/app/pages/dashboardPage/dashboardPage.tsx
+++ src/app/pages/dashboardPage/dashboardPage.tsx
@@ -1,20 +1,40 @@
 import * as React from 'react';
+import { motion } from 'framer-motion';
 import LottiePlayer from 'lotty/lottiePlayer';
 import deskAnimation from 'lotty/lotties/desk.json';
 import fakeApi from 'api/fakeApi';
 import * as styles from './dashboardPage.module.scss';
 
+// const DashboardPage: React.FC = () => {
+//   fakeApi.sampleData.read();
+//   return (
+//     <section className={styles.section}>
+//       <LottiePlayer
+//         animationData={deskAnimation}
+//         autoplay
+//         loop
+//         size={600}
+//       />
+//     </section>
+//   );
+// };
+
+const variants = {
+  on: { top: '3rem' },
+  off: { top: '0' },
+};
+
 const DashboardPage: React.FC = () => {
-  fakeApi.sampleData.read();
+  const [state, setState] = React.useState(false);
   return (
-    <section className={styles.section}>
-      <LottiePlayer
-        animationData={deskAnimation}
-        autoplay
-        loop
-        size={600}
+    <>
+      <motion.div
+        animate={state ? 'on' : 'off'}
+        className={styles.div}
+        variants={variants}
       />
-    </section>
+      <button onClick={() => setState(!state)} type="button">Click Me</button>
+    </>
   );
 };
 
