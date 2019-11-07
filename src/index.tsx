04137431d23da1ae07d18c336e5f31517a7013b2
Jimmy Emery <jemery@allcampus.com>
2019-11-06 22:25:13 -0500

    figuring out framer motion routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
 import 'babel-polyfill';
 import * as React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
-import Spa from './app/spa';
+// import Spa from './app/spa';
+// import SpaTwo from './app/spaTwo';
+import SpaThree from './app/spaThree';
+
+// render(
+//   <BrowserRouter>
+//     <Spa />
+//   </BrowserRouter>,
+//   document.getElementById('app'),
+// );
+
+// render(
+//   <SpaTwo />,
+//   document.getElementById('app'),
+// );
 
 render(
-  <BrowserRouter>
-    <Spa />
-  </BrowserRouter>,
+  <SpaThree />,
   document.getElementById('app'),
 );
