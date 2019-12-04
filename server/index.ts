efcb1a945973d8b2d0afb426080de73c23844248
Jimmy Emery <jemery@allcampus.com>
2019-12-03 22:31:08 -0500

    updating server

--- server/index.ts
+++ server/index.ts
@@ -1,30 +1,26 @@
-// import express from 'express';
-// import { ApolloServer, gql } from 'apollo-server-express';
-//
-// // type definitions (schema)
-// const typeDefs = gql`
-//   type Query {
-//     hello: String
-//   }
-// `;
-//
-// // resolvers
-// const resolvers = {
-//   Query: {
-//     hello: () => 'Hello world!',
-//   },
-// };
-//
-// const server = new ApolloServer({ typeDefs, resolvers });
-//
-// const app = express();
-// server.applyMiddleware({ app });
-//
-// app.listen(
-//   { port: 4000 },
-//   () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
-// );
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-console.log('foo');
-const bar = (): string => 'hello';
-console.log(bar());
+// type definitions (schema)
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// resolvers
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app });
+
+app.listen(
+  { port: 4000 },
+  () => console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
