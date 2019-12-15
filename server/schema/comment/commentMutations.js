7a1547865fb7d023a57bc5bce06ccfd67f98102f
Jimmy Emery <jemery@allcampus.com>
2019-12-15 16:10:12 -0500

    updating server

--- server/schema/comment/commentMutations.js
+++ server/schema/comment/commentMutations.js
@@ -18,22 +18,34 @@ export default {
     };
 
     db.comments.push(comment);
-    pubsub.publish(`comment ${args.data.post}`, { comment });
+    pubsub.publish(`comment ${args.data.post}`, {
+      comment: {
+        data: comment,
+        mutation: 'CREATED',
+      },
+    });
 
     return comment;
   },
-  deleteComment: (parent, args, { db }, info) => {
+  deleteComment: (parent, args, { db, pubsub }, info) => {
     const commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
 
     if (commentIndex === -1) {
       throw new Error('Comment not found.');
     }
 
-    const deletedComment = db.comments.splice(commentIndex, 1);
+    const [deletedComment] = db.comments.splice(commentIndex, 1);
 
-    return deletedComment[0];
+    pubsub.publish(`comment ${deletedComment.post}`, {
+      comment: {
+        data: deletedComment,
+        mutation: 'DELETED',
+      },
+    });
+
+    return deletedComment;
   },
-  updateComment: (parent, { id, data }, { db }, info) => {
+  updateComment: (parent, { id, data }, { db, pubsub }, info) => {
     const comment = db.comments.find((c) => c.id === id);
 
     if (!comment) {
@@ -43,6 +55,13 @@ export default {
     if (typeof data.text === 'string') {
       comment.text = data.text;
     }
+
+    pubsub.publish(`comment ${comment.post}`, {
+      comment: {
+        data: comment,
+        mutation: 'DELETED',
+      },
+    });
 
     return comment;
   },
