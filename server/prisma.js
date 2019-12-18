0751c255a46af9b8844be1a4437d5657aaa09cc7
Jimmy Emery <jemery@allcampus.com>
2019-12-17 22:26:35 -0500

    updating server

--- server/prisma.js
+++ server/prisma.js
@@ -5,6 +5,62 @@ const prisma = new Prisma({
   typeDefs: 'server/generated/prisma.graphql',
 });
 
-prisma.query.users(null, '{ id name email }').then((data) => {
-  console.log(data);
-});
+const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({ id: authorId });
+
+  if (!userExists) {
+    throw new Error('User not found.');
+  }
+
+  const post = await prisma.mutation.createPost({
+    data: {
+      ...data,
+      author: {
+        connect: {
+          id: authorId,
+        },
+      },
+    },
+  }, '{ author { id name email posts { id title published } } }');
+
+  return post.author;
+};
+
+// createPostForUser('ck4aoi61d002m0896w9ujg64f', {
+//   title: 'Great books to read',
+//   body: 'The War of Art',
+//   published: true,
+// }).then((user) => {
+//   console.log(JSON.stringify(user, undefined, 2));
+// }).catch((error) => {
+//   console.log(error.message);
+// });
+
+const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({ id: postId });
+
+  if (!postExists) {
+    throw new Error('Post not found.');
+  }
+
+  const post = await prisma.mutation.updatePost(
+    {
+      where: {
+        id: postId,
+      },
+      data,
+    },
+    '{ author { id name email posts { id title published } } }',
+  );
+
+  return post.author;
+};
+
+// updatePostForUser(
+//   'ck4aojt3l004e08962f9x61s8',
+//   { published: false },
+// ).then((user) => {
+//   console.log(JSON.stringify(user, undefined, 2));
+// }).catch((error) => {
+//   console.log(error.message);
+// });
