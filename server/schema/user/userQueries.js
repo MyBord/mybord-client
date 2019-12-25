04372a71094483582315525162709c7187237039
Jimmy Emery <jemery@allcampus.com>
2019-12-24 22:04:42 -0500

    updating user query

--- server/schema/user/userQueries.js
+++ server/schema/user/userQueries.js
@@ -14,12 +14,32 @@ export default {
   },
   users: async (parent, args, { prisma, request }, info) => {
     const userId = getUserId(request, false);
-    const users = await prisma.query.users(args, info);
+
+    // You shouldn't be able to run a filtered query where you ask for a particular set of
+    // restricted fields are then returned that user, if that user, with said email address,
+    // exists in the db. This essentially is can serve as a loophole to see if certain users
+    // exist given the presence of certain restricted fields. These restricted fields are:
+    // - user email address
+    const finalArgs = {
+      ...args,
+      where: {
+        ...args.where,
+      },
+    };
+    if (args && args.where && args.where.email) {
+      delete finalArgs.where.email;
+    }
+
+    const users = await prisma.query.users(finalArgs, info);
+
+    // return restricted user data
     return users.map(async (user) => {
+      // allow the authenticated user to see all of their own data
       if (userId && userId === user.id) {
         return user;
       }
 
+      // a user can not see the email address or un-published posts of other users
       const userPostsArgs = {
         where: {
           author: { id: user.id },
