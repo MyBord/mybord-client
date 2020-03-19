e2717af2ff3441ba56529278f074f702d07fc1e0
Jimmy Emery <jemery@allcampus.com>
2020-03-18 20:33:03 -0400

    adding spinning button

--- src/spa/shared/buttons/button/button.tsx
+++ src/spa/shared/buttons/button/button.tsx
@@ -9,6 +9,7 @@ interface Props {
   htmlType?: 'button' | 'reset' | 'submit';
   iconName?: IconNames;
   label: string;
+  loading?: boolean;
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   type?: 'primary' | 'secondary';
 }
@@ -17,12 +18,14 @@ const Button: React.FC<Props> = ({
   htmlType = 'button',
   iconName = null,
   label,
+  loading = false,
   onClick,
   type = 'primary',
 }) => (
   <AntButton
     className={[styles.button, styles[type]].join(' ')}
     htmlType={htmlType}
+    loading={loading}
     onClick={onClick}
     type={type === 'primary' ? 'primary' : null}
   >
