34f8debdc536d2a379bafd8aa433158e179fcf16
Jimmy Emery <jemery@allcampus.com>
2020-04-24 14:16:55 -0400

    udpating trending page

--- src/spa/app/pages/trendingPage/trendingPage.tsx
+++ src/spa/app/pages/trendingPage/trendingPage.tsx
@@ -1,34 +1,14 @@
 import * as React from 'react';
-import api, { Resource } from 'api/api';
-import { Foo } from 'schema/user';
 import LottiePlayer from 'lotty/lottiePlayer';
 import trendingAnimation from 'lotty/lotties/trending.json';
 
-interface Props {
-  resource: Resource<void>;
-}
+const TrendingPage: React.FC = () => (
+  <LottiePlayer
+    animationData={trendingAnimation}
+    autoplay
+    loop
+    size={600}
+  />
+);
 
-const TrendingPageContainer: React.FC = () => {
-  const resource = api.query(Foo);
-  return (
-    <React.Suspense fallback={<h1>Loading...</h1>}>
-      <TrendingPageComponent resource={resource} />
-    </React.Suspense>
-  );
-};
-
-const TrendingPageComponent: React.FC<Props> = ({ resource }) => {
-  const data = resource.data.read();
-  console.log(' -- resource data: --');
-  console.log(data);
-  return (
-    <LottiePlayer
-      animationData={trendingAnimation}
-      autoplay
-      loop
-      size={600}
-    />
-  );
-};
-
-export default TrendingPageContainer;
+export default TrendingPage;
