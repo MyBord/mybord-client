b45b800a82e74fa1c7ed0d219fb96225ae7ea4bf
Jimmy Emery <jemery@allcampus.com>
2020-05-09 15:54:57 -0400

    updating init wrapper

--- src/spa/spa/initializeSpaWrapper.tsx
+++ src/spa/spa/initializeSpaWrapper.tsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useQuery } from '@apollo/react-hooks';
 import SpaFallback from 'fallbacks/spaFallback/spaFallback';
+import { IS_AUTHENTICATED } from 'schema/user';
+import { getTwoChildOpacityTransition } from 'framerMotion/animationVariants';
+import { useAuthenticationContext } from 'context/authenticationContext';
+import * as styles from './spa.module.less';
 
 // This wrapper is responsible for initializing our SPA. Once all fetches, actions, etc have
 // been performed, then the actual SPA can be rendered. In the meantime, we will load a spinner
@@ -8,7 +14,45 @@ import SpaFallback from 'fallbacks/spaFallback/spaFallback';
 //     / has an existing session or not.
 // Note: We are not using Suspense here because we cannot have transitions between fallbacks.
 const initializeSpaWrapper = (WrappedComponent: React.FC): React.FC => {
-  const WrappedSpa: React.FC = () => <SpaFallback />;
+  const WrappedSpa: React.FC = () => {
+    const { called, data, loading } = useQuery(IS_AUTHENTICATED);
+    const [isInitializationComplete, setIsInitializationComplete] = React.useState(false);
+    const { isAuthenticated, setAuthenticationStatus } = useAuthenticationContext();
+
+    if (called && !loading && !isInitializationComplete) {
+      if (data.isAuthenticated) {
+        setAuthenticationStatus(true);
+      } else {
+        setAuthenticationStatus(false);
+      }
+      setIsInitializationComplete(true);
+    }
+
+    const animationVariants = getTwoChildOpacityTransition(1.0);
+
+    return (
+      <AnimatePresence>
+        <motion.div
+          animate="enter"
+          className={styles.spaDiv}
+          exit="exit"
+          initial="initial"
+          key={isInitializationComplete ? 'initialized' : 'initializing'}
+          variants={
+            isInitializationComplete ? animationVariants.lastChild : animationVariants.firstChild
+          }
+        >
+          <div className={styles.spaDivChild}>
+            {
+              (isInitializationComplete && isAuthenticated !== null)
+                ? <WrappedComponent />
+                : <SpaFallback />
+            }
+          </div>
+        </motion.div>
+      </AnimatePresence>
+    );
+  };
 
   return WrappedSpa;
 };
