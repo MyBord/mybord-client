c876dc85199f3835b200af74d06448a59c13aea4
Jimmy Emery <jemery@allcampus.com>
2020-05-11 10:53:09 -0400

    updating

--- src/spa/app/app.tsx
+++ src/spa/app/app.tsx
@@ -1,14 +1,32 @@
 import * as React from 'react';
-import ErrorBoundary from 'app/errorBoundary/errorBoundary';
-import Layout from 'layout/layout';
-import Routes from 'routing/routes';
+import { fetchProfileData } from 'api/fakeApi';
 
-const App: React.FC = () => (
-  <Layout>
-    <ErrorBoundary>
-      <Routes />
-    </ErrorBoundary>
-  </Layout>
-);
+const resource = fetchProfileData();
+
+const App: React.FC = () => {
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  return (
+    <>
+      <h1 style={{ margin: '3rem' }}>{isLoaded ? 'LOADED' : 'NOT LOADED'}</h1>
+      <React.Suspense fallback={<h1>Loading profile...</h1>}>
+        <AppDetails setIsLoaded={setIsLoaded} />
+      </React.Suspense>
+    </>
+  );
+};
+
+interface Props {
+  setIsLoaded: (status: boolean) => void;
+}
+
+const AppDetails: React.FC<Props> = ({ setIsLoaded }) => {
+  const todo = resource.cards.read();
+  console.log('**********');
+  console.log(todo);
+
+  React.useEffect(() => setIsLoaded(true), []);
+
+  return <h1>hello world</h1>;
+};
 
 export default App;
