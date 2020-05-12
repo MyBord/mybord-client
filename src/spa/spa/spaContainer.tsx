61d503e9bfc9d4fe0f4cc328d81518b30b733411
Jimmy Emery <jemery@allcampus.com>
2020-05-11 21:00:51 -0400

    adding spa animation

--- src/spa/spa/spaContainer.tsx
+++ src/spa/spa/spaContainer.tsx
@@ -1,65 +1,19 @@
 import * as React from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
 import App from 'app/app';
 import Landing from 'landing/landing';
-import { getTwoChildOpacityTransition } from 'framerMotion/commonVariants';
+import SpaAnimation from 'framerMotion/spaAnimation';
 import { useAuthenticationContext } from 'context/authenticationContext';
 import initializeSpaWrapper from './initializeSpaWrapper';
-import * as styles from './spa.module.less';
-
-const variants = {
-  initial: {
-    opacity: 0,
-  },
-  enter: {
-    opacity: 1,
-    transition: { duration: 1.0 },
-  },
-  exit: {
-    opacity: 0,
-    transition: { duration: 1.0 },
-  },
-};
 
 // This component renders the application with a landing page or the actual application based
 // on the users authentication status.
 const SpaContainer: React.FC = () => {
   const { isAuthenticated } = useAuthenticationContext();
   return (
-    <AnimatePresence>
-      {
-        isAuthenticated && (
-          <motion.div
-            animate="enter"
-            className={styles.spa}
-            exit="exit"
-            initial="initial"
-            key="isAuthenticated"
-            variants={variants}
-          >
-            <div className={styles.div}>
-              <App />
-            </div>
-          </motion.div>
-        )
-      }
-      {
-        !isAuthenticated && (
-          <motion.div
-            animate="enter"
-            className={styles.spa}
-            exit="exit"
-            initial="initial"
-            key="isNotAuthenticated"
-            variants={variants}
-          >
-            <div className={styles.div}>
-              <Landing />
-            </div>
-          </motion.div>
-        )
-      }
-    </AnimatePresence>
+    <SpaAnimation isAuthenticated={isAuthenticated}>
+      <App />
+      <Landing />
+    </SpaAnimation>
   );
 };
 
--- src/thirdParty/framerMotion/spaAnimation.tsx
+++ src/thirdParty/framerMotion/spaAnimation.tsx
@@ -1,29 +1,61 @@
 import * as React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { getTwoChildOpacityTransition } from './commonVariants';
 import * as styles from './animations.module.less';
 
+const variants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    transition: { duration: 1.0 },
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 1.0 },
+  },
+};
+
 interface Props {
-  children: React.ReactNode;
+  children: React.ReactNodeArray;
   isAuthenticated: boolean;
 }
 
-const SpaAnimation: React.FC<Props> = ({ children, isAuthenticated }) => {
-  const animationVariants = getTwoChildOpacityTransition(1.0);
-  return (
-    <AnimatePresence>
-      <motion.div
-        animate="enter"
-        className={styles.spa}
-        exit="exit"
-        initial="initial"
-        key={isAuthenticated ? 'isAuthenticated' : 'notAuthenticated'}
-        variants={isAuthenticated ? animationVariants.lastChild : animationVariants.firstChild}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  );
-};
+const SpaAnimation: React.FC<Props> = ({ children, isAuthenticated }) => (
+  <AnimatePresence>
+    {
+      isAuthenticated && (
+        <motion.div
+          animate="enter"
+          className={styles.spaParentDiv}
+          exit="exit"
+          initial="initial"
+          key="isAuthenticated"
+          variants={variants}
+        >
+          <div className={styles.spaChildDiv}>
+            {children[0]}
+          </div>
+        </motion.div>
+      )
+    }
+    {
+      !isAuthenticated && (
+        <motion.div
+          animate="enter"
+          className={styles.spaParentDiv}
+          exit="exit"
+          initial="initial"
+          key="isNotAuthenticated"
+          variants={variants}
+        >
+          <div className={styles.spaChildDiv}>
+            {children[1]}
+          </div>
+        </motion.div>
+      )
+    }
+  </AnimatePresence>
+);
 
 export default SpaAnimation;
