233a2f6428c3069127282551ad367f55c587b1d2
Jimmy Emery <jemery@allcampus.com>
2020-05-12 16:39:26 -0400

    updating buttons

--- src/spa/app/layout/header/header.tsx
+++ src/spa/app/layout/header/header.tsx
@@ -1,13 +1,28 @@
 import * as React from 'react';
+import { useLazyQuery } from '@apollo/react-hooks';
+import Button from 'buttons/button/button';
 import HeaderAnimation from 'framerMotion/headerAnimation';
+import { LOGOUT_USER } from 'schema/user';
+import { useAuthenticationContext } from 'context/authenticationContext';
 import HeaderTitles from './headerTitles/headerTitles';
 import HeaderTools from './headerTools/headerTools';
 
-const Header: React.FC = () => (
-  <HeaderAnimation>
-    <HeaderTitles />
-    <HeaderTools />
-  </HeaderAnimation>
-);
+const Header: React.FC = () => {
+  const [LogoutUserQuery] = useLazyQuery(LOGOUT_USER);
+  const { setAuthenticationStatus } = useAuthenticationContext();
+
+  const handleClick = async (): Promise<void> => {
+    await LogoutUserQuery();
+    setAuthenticationStatus(false);
+  };
+
+  return (
+    <HeaderAnimation>
+      <Button onClick={handleClick} label="Logout" />
+      <HeaderTitles />
+      <HeaderTools />
+    </HeaderAnimation>
+  );
+};
 
 export default Header;
