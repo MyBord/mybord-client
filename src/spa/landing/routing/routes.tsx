53219fd0cfbbc681a5edfdffbeb8b8d1530fc92c
Jimmy Emery <jemery@allcampus.com>
2020-05-15 16:46:48 -0400

    adding about page

--- src/spa/landing/routing/routes.tsx
+++ src/spa/landing/routing/routes.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import AboutPage from 'landing/pages/aboutPage/aboutPage';
 import ErrorPage from 'landing/pages/errorPage/errorPage';
 import LoginPage from 'landing/pages/loginPage/loginPage';
 
@@ -7,6 +8,9 @@ const Routes: React.FC = () => (
   <Switch>
     <Route exact path="/">
       <LoginPage />
+    </Route>
+    <Route exact path="/about">
+      <AboutPage />
     </Route>
     <Route exact path="/error">
       <ErrorPage />
