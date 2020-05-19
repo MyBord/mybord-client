f36086259244471c67035f23410823bb35540d02
Jimmy Emery <jemery@allcampus.com>
2020-05-19 17:40:52 -0400

    updating form

--- src/spa/shared/forms/form/form.tsx
+++ src/spa/shared/forms/form/form.tsx
@@ -1,21 +1,32 @@
 import * as React from 'react';
 import { Form as AntForm } from 'antd';
 import { FormProp, FormProps } from 'types/formTypes';
+import * as styles from './form.module.less';
 
 // todo: change name of file & folder
 
 interface FormContentProps extends FormProps {
   children: React.ReactElement | React.ReactElement[];
   onSubmit: (form: FormProp) => void;
+  type?: 'login';
 }
 
 interface Props {
   children: React.ReactElement | React.ReactElement[];
   onSubmit: (form: FormProp) => void;
+  type?: 'login';
 }
 
 const FormContent = React.forwardRef<FormProps, FormContentProps>(
-  ({ children, form, onSubmit }: FormContentProps, ref) => {
+  (
+    {
+      children,
+      form,
+      onSubmit,
+      type,
+    }: FormContentProps,
+    ref,
+  ) => {
     React.useImperativeHandle(ref, () => ({
       form,
     }));
@@ -30,7 +41,11 @@ const FormContent = React.forwardRef<FormProps, FormContentProps>(
     };
 
     return (
-      <AntForm onSubmit={handleSubmit}>
+      <AntForm
+        className={type ? styles[type] : undefined}
+        hideRequiredMark
+        onSubmit={handleSubmit}
+      >
         {
           React.Children.map(children, (child) => React.cloneElement(child, { form }))
         }
@@ -42,8 +57,12 @@ const FormContent = React.forwardRef<FormProps, FormContentProps>(
 const FormInstance = AntForm.create<FormContentProps>()(FormContent);
 
 const Form = React.forwardRef<FormProps, Props>(
-  ({ children, onSubmit }: Props, ref) => (
-    <FormInstance onSubmit={onSubmit} wrappedComponentRef={ref}>
+  ({ children, onSubmit, type = null }: Props, ref) => (
+    <FormInstance
+      onSubmit={onSubmit}
+      type={type}
+      wrappedComponentRef={ref}
+    >
       {children}
     </FormInstance>
   ),
