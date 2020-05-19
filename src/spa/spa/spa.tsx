340d7b6a92c3dc3856853de4f65c31f553b118b8
Jimmy Emery <jemery@allcampus.com>
2020-05-19 13:41:12 -0400

    updating / reverting spa

--- src/spa/spa/spa.tsx
+++ src/spa/spa/spa.tsx
@@ -1,46 +1,18 @@
 import * as React from 'react';
-import { Button, Input, Divider } from 'antd';
-import Form, { FormComponentProps } from 'antd/lib/form/Form';
-import formWrapper from 'forms/formWrapper/formWrapperTwo';
-import 'antd/dist/antd.css';
+import { BrowserRouter } from 'react-router-dom';
+import SpaProviders from './spaProviders';
+import SpaContainer from './spaContainer';
 
-const Spa: React.FC = () => {
-  const formRef = React.createRef<FormComponentProps>();
-  const [inputValue, setInputValue] = React.useState();
-  const [state, set] = React.useState(false);
-
-  const handleSubmit = (): void => {
-    setInputValue(formRef.current.form.getFieldValue('name'));
-  };
-
-  // const FormConent = () => (
-  //   <>
-  //     <Form.Item label="name">
-  //       {form.getFieldDecorator('name')(<Input />)}
-  //     </Form.Item>
-  //     <Button htmlType="submit">submit</Button>
-  //   </>
-  // );
-
-  const FormContent: React.FC = () => (
-    <>
-      <Button htmlType="submit">submit</Button>
-    </>
-  );
-
-  const FinalForm = formWrapper(FormContent);
-
-  return (
-    <div>
-      <button type="button" onClick={() => set((i) => !i)}>
-        Click Me
-      </button>
-      <h3>{state ? 'TRUE' : 'FALSE'}</h3>
-      <div>{`I'm the value from form: ${inputValue}`}</div>
-      <Divider />
-      <FinalForm onSubmit={() => console.log('foo')} />
-    </div>
-  );
-};
+// This is the actual final, production React code that instantiates our application. It contains:
+//   * A router
+//   * Context Providers
+//   * Our actual spa application
+const Spa: React.FC = () => (
+  <BrowserRouter>
+    <SpaProviders>
+      <SpaContainer />
+    </SpaProviders>
+  </BrowserRouter>
+);
 
 export default Spa;
