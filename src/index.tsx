827388193476a297c4a9e0fc64c25086c1091d5f
Jimmy Emery <jemery@allcampus.com>
2020-05-21 14:07:37 -0400

    adding client folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,12 @@
 import 'babel-polyfill';
 import * as React from 'react';
-import { ApolloClient } from 'apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { WebSocketLink } from 'apollo-link-ws';
 // @ts-ignore
 import { createRoot } from 'react-dom';
-import { getMainDefinition } from 'apollo-utilities';
-import { split } from 'apollo-link';
+import { client } from 'server/client';
 import Spa from './spa/spa/spa';
 
-const wsLink = new WebSocketLink({
-  uri: `ws://${process.env.URI}`,
-  options: { reconnect: true },
-});
-
-const httpLink = new HttpLink({
-  credentials: 'include',
-  uri: `http://${process.env.URI}`,
-});
-
-const link = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition'
-      && definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink,
-);
-
-const client = new ApolloClient({
-  link,
-  cache: new InMemoryCache(),
-});
-
-createRoot(
-  document.getElementById('app'),
-).render(
+createRoot(document.getElementById('app')).render(
   <ApolloProvider client={client}>
     <Spa />
   </ApolloProvider>,
---
+++ src/server/client.tsx
@@ -0,0 +1,35 @@
+// Source: https://www.apollographql.com/docs/react/data/subscriptions/#client-setup
+
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { split } from 'apollo-link';
+
+const wsLink = new WebSocketLink({
+  uri: `ws://${process.env.URI}`,
+  options: { reconnect: true },
+});
+
+const httpLink = new HttpLink({
+  credentials: 'include',
+  uri: `http://${process.env.URI}`,
+});
+
+const link = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition'
+      && definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
