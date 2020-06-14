74bbcd9454d8dab2b239313f847f847f31e5db4e
Jimmy Emery <jemery@allcampus.com>
2020-06-14 12:38:20 -0400

    updating api

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,15 +7,13 @@ export interface Resource<T> {
 }
 
 const get = (gqlString: GqlString): Promise<any> => new Promise((resolve) => {
-  setTimeout(() => {
-    const body = JSON.stringify({ query: gqlString.loc.source.body });
-    const config = {
-      headers: { 'Content-Type': 'application/json' },
-      withCredentials: true,
-    };
-    axios.post(`http://${process.env.URI}`, body, config)
-      .then((response) => resolve(response.data.data));
-  }, 2000);
+  const body = JSON.stringify({ query: gqlString.loc.source.body });
+  const config = {
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
+  };
+  axios.post(`http://${process.env.URI}`, body, config)
+    .then((response) => resolve(response.data.data));
 });
 
 const query = (gqlString: GqlString): Resource<any> => {
