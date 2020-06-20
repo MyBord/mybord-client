2cb9ff257dbb18f2c82700eef3238a3eb371703c
Jimmy Emery <jemery@allcampus.com>
2020-06-20 13:23:17 -0400

    updating

--- src/spa/app/pages/dashboardPage/dashboardCards/dashboardCardsContainer.tsx
+++ src/spa/app/pages/dashboardPage/dashboardCards/dashboardCardsContainer.tsx
@@ -1,38 +1,55 @@
 import * as React from 'react';
+import { pull } from 'lodash';
 import { useSubscription } from '@apollo/react-hooks';
 import {
   DELETED_USER_CARD_SUBSCRIPTION,
   USER_CARD_SUBSCRIPTION,
   UserCard,
-  UserCardsQueryResponse,
 } from 'schema/card';
 import DashboardCardsComponent from './dashboardCardsComponent';
 
 interface Props {
-  userCards: UserCardsQueryResponse['userCards'];
+  userCards: UserCard[];
 }
 
 const DashboardCardsContainer: React.FC<Props> = ({ userCards }) => {
-  const userCardSubscription = useSubscription(USER_CARD_SUBSCRIPTION);
-  const deletedUserCardSubscription = useSubscription(DELETED_USER_CARD_SUBSCRIPTION);
-  const finalUserCards = [...userCards];
-  const userCardsIds = finalUserCards.map((userCard: UserCard) => userCard.id);
+  const { data: cardData, loading: cardLoading } = useSubscription(USER_CARD_SUBSCRIPTION);
+  const {
+    data: deleteData,
+    loading: deleteLoading,
+  } = useSubscription(DELETED_USER_CARD_SUBSCRIPTION);
 
-  // if (
-  //   !deletedUserCardSubscription.loading
-  //   && userCardsIds.includes(deletedUserCardSubscription.data.deletedUserCard.id)
-  // ) {
-  //   const index = userCardsIds.indexOf(deletedUserCardSubscription.data.deletedUserCard.id);
-  //   userCards.splice(index, 1);
-  //   // userCardsIds.splice(index, 1);
-  // }
+  const [finalUserCards, setFinalUserCards] = React.useState<UserCard[]>([...userCards]);
+  const [
+    userCardsIds,
+    setUserCardIds,
+  ] = React.useState<string[]>(finalUserCards.map((userCard: UserCard) => userCard.id));
+
+  // We need to track the array of `deletedCardIds` so that in the case where the user adds a
+  // new card *and* deletes it within the same browser / spa session, we do not create an
+  // infinite loop where we delete the card but then we detect that the card is missing in
+  // `userCardsIds` and thus add it back in.
+  const [deletedCardIds, setDeletedCardIds] = React.useState<string[]>([]);
 
   if (
-    !userCardSubscription.loading
-    && !userCardsIds.includes(userCardSubscription.data.userCard.id)
+    !deleteLoading
+    && userCardsIds.includes(deleteData.deletedUserCard.id)
   ) {
-    finalUserCards.push(userCardSubscription.data.userCard);
-    // userCardsIds.push(userCardSubscription.data.userCard.id);
+    setFinalUserCards((prevState) => {
+      prevState.splice(userCardsIds.indexOf(deleteData.deletedUserCard.id), 1);
+      return prevState;
+    });
+    setUserCardIds((prevState) => pull(prevState, deleteData.deletedUserCard.id));
+    setDeletedCardIds((prevState) => prevState.concat(deleteData.deletedUserCard.id));
+  }
+
+  if (
+    !cardLoading
+    && !userCardsIds.includes(cardData.userCard.id)
+    && !deletedCardIds.includes(cardData.userCard.id)
+  ) {
+    setFinalUserCards((prevState) => prevState.concat(cardData.userCard));
+    setUserCardIds((prevState) => prevState.concat(cardData.userCard.id));
   }
 
   return <DashboardCardsComponent userCards={finalUserCards} />;
