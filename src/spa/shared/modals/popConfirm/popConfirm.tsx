aa3acbedc91f465febb21eedaa07131badf3d87d
Jimmy Emery <jemery@allcampus.com>
2020-06-20 18:58:54 -0400

    creating pop confirm component

---
+++ src/spa/shared/modals/popConfirm/popConfirm.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as styles from './popConfirm.module.less';
+
+const PopConfirm: React.FC = () => (
+  <div className={styles.container}>
+    <h1>hello world</h1>
+  </div>
+);
+
+export default PopConfirm;
