c722234992686a24d7155078f8d1b8e708ee5fd1
Jimmy Emery <jemery@allcampus.com>
2020-06-21 17:46:03 -0400

    updating favoriting in card content

--- src/spa/shared/cards/card/cardContent/cardContent.tsx
+++ src/spa/shared/cards/card/cardContent/cardContent.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import CardContentButtonsAnimation from 'framerMotion/cardContentButtonsAnimation';
 import CardMenuButtonContainer from 'buttons/cardMenuButton/cardMenuButtonContainer';
 import LikeButton from 'buttons/likeButton/likeButton';
-import { UserCard } from 'schema/card';
+import { TOGGLE_FAVORITE_USER_CARD, UserCard } from 'schema/card';
 import { useCardContext } from 'context/cardContext';
 import * as styles from './cardContent.module.less';
 
@@ -16,7 +17,18 @@ const CardContent: React.FC<Props> = ({
   userCard,
 }) => {
   const containerRef = React.useRef(null);
+  const [isFavorite, setIsFavorite] = React.useState<boolean>(userCard.isFavorite);
   const { activeCard, canMultiEdit } = useCardContext();
+  const [toggleFavoriteUserCard] = useMutation(TOGGLE_FAVORITE_USER_CARD);
+
+  const handleFavorite = async (): Promise<void> => {
+    await toggleFavoriteUserCard({
+      variables: {
+        cardId: userCard.id,
+      },
+    });
+    setIsFavorite((prevState) => !prevState);
+  };
 
   const showButtons = !canMultiEdit && activeCard.id !== userCard.id;
 
@@ -24,8 +36,8 @@ const CardContent: React.FC<Props> = ({
     <div className={styles.container} ref={containerRef}>
       <CardContentButtonsAnimation showButtons={showButtons}>
         <LikeButton
-          isLiked={userCard.isFavorite}
-          onClick={() => console.log('foo')}
+          isLiked={isFavorite}
+          onClick={handleFavorite}
           size={25}
         />
         <CardMenuButtonContainer cardId={userCard.id} containerRef={containerRef} />
