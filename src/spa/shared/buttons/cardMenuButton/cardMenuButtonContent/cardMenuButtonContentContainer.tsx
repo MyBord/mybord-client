ac589a473e49ded3107896db05eb2ccfebd3c12b
Jimmy <jimmy.e@outlook.com>
2020-06-22 20:22:42 -0400

    Todo handshake (#57)
    * updating
    
    * updating
    
    * updating
    
    * updating

--- src/spa/shared/buttons/cardMenuButton/cardMenuButtonContent/cardMenuButtonContentContainer.tsx
+++ src/spa/shared/buttons/cardMenuButton/cardMenuButtonContent/cardMenuButtonContentContainer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useMutation } from '@apollo/react-hooks';
-import { DELETE_USER_CARD, UserCard } from 'schema/card';
+import { DELETE_USER_CARD, TOGGLE_TO_DO_USER_CARD, UserCard } from 'schema/card';
 import CardMenuButtonContentComponent from './cardMenuButtonContentComponent';
 
 interface Props {
@@ -8,16 +8,28 @@ interface Props {
 }
 
 const CardMenuButtonContentContainer: React.FC<Props> = ({ userCard }) => {
-  const [deleteYoutubeCard] = useMutation(DELETE_USER_CARD);
-  const [isToDo, setIsToDo] = React.useState<boolean>(true);
+  const [deleteUserCard] = useMutation(DELETE_USER_CARD);
+  const [toggleToDoUserCard] = useMutation(TOGGLE_TO_DO_USER_CARD);
+  const [isToDo, setIsToDo] = React.useState<boolean>(userCard.isToDo);
   const [showPopConfirm, setShowPopConfirm] = React.useState<boolean>(false);
   const deleteButtonRef = React.useRef<HTMLDivElement>(null);
-  const toggleToDo = (): void => setIsToDo((prevState) => !prevState);
 
   const handleDelete = async (): Promise<void> => {
-    await deleteYoutubeCard({
-      variables: { id: userCard.id },
+    await deleteUserCard({
+      variables: { cardId: userCard.id },
     });
+  };
+
+  const toggleToDo = async (): Promise<void> => {
+    setIsToDo((prevState) => !prevState);
+    try {
+      await toggleToDoUserCard({
+        variables: { cardId: userCard.id },
+      });
+    } catch (error) {
+      setIsToDo((prevState) => !prevState);
+      throw new Error(error);
+    }
   };
 
   return (
