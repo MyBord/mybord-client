cc7abd04efbfdce1672413b4cc88a394c4a0f438
Jimmy <jimmy.e@outlook.com>
2020-06-23 20:03:21 -0400

    updating (#58)

--- src/spa/shared/inputs/toggle/toggle.tsx
+++ src/spa/shared/inputs/toggle/toggle.tsx
@@ -1,23 +1,54 @@
 import * as React from 'react';
 import { Switch } from 'antd';
+import Typography from 'typography/typography';
+import * as styles from './toggle.module.less';
 import './toggle.less';
 
 interface Props {
   checked: boolean;
   onClick: () => void;
   size?: 'large' | 'small';
+  text?: string;
 }
 
 const Toggle: React.FC<Props> = ({
   checked,
   onClick,
   size = 'large',
-}) => (
-  <Switch
-    checked={checked}
-    onClick={onClick}
-    size={size === 'large' ? 'default' : size}
-  />
-);
+  text = null,
+}) => {
+  if (text) {
+    return (
+      <button
+        className={styles.button}
+        onClick={onClick}
+        type="button"
+      >
+        <Switch
+          checked={checked}
+          size={size === 'large' ? 'default' : size}
+        />
+        <Typography
+          color="blue"
+          size={size === 'large' ? 'normal' : 'two'}
+          text={text}
+        />
+      </button>
+    );
+  }
+
+  return (
+    <button
+      className={styles.button}
+      onClick={onClick}
+      type="button"
+    >
+      <Switch
+        checked={checked}
+        size={size === 'large' ? 'default' : size}
+      />
+    </button>
+  );
+};
 
 export default Toggle;
