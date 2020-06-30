da9629d4783fc2dc46db97111b86c3e27360108d
Jimmy Emery <jemery@allcampus.com>
2020-06-30 12:00:56 -0400

    udpating ellipsis

--- src/spa/shared/typography/typographyText/typographyText.tsx
+++ src/spa/shared/typography/typographyText/typographyText.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
+import { TypographySize } from 'types/typographyTypes';
 import * as styles from './typographyText.module.less';
 
 interface Props {
   Content?: React.FC;
   maxTextLength?: number;
+  size: TypographySize;
   text: string;
 }
 
 const TypographyText: React.FC<Props> = ({
   Content,
   maxTextLength,
+  size,
   text,
 }) => {
   const renderText = (): React.ReactElement => {
@@ -18,7 +21,7 @@ const TypographyText: React.FC<Props> = ({
         <>
           <p className={styles.p}>
             {text.slice(0, maxTextLength)}
-            <div className={styles.ellipsis}>
+            <div className={[styles.ellipsis, styles[size]].join(' ')}>
               <div className={styles.dot} />
               <div className={styles.dot} />
               <div className={styles.dot} />
