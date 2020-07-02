8c27d043ef08cae9dcffbbb741fd79196016515d
Jimmy Emery <jemery@allcampus.com>
2020-07-02 15:39:46 -0400

    updating

--- src/thirdParty/framerMotion/popOverAnimation.tsx
+++ src/thirdParty/framerMotion/popOverAnimation.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { PopOverProps, PopOverStyle } from 'types/modalTypes';
+import getPopOverStyle from 'modals/popOver/getPopOverStyle';
 import * as styles from './popOverAnimation.module.less';
 
 const variants = {
@@ -19,36 +20,53 @@ const variants = {
 };
 
 interface Props {
+  caretPlacement: PopOverProps['caretPlacement'];
   children: PopOverProps['children'];
   showPopOver: boolean;
-  style?: PopOverStyle;
+  placement: PopOverProps['placement'];
+  popOverRef: React.RefObject<HTMLDivElement>;
 }
 
-const PopOverAnimation = React.forwardRef<HTMLDivElement, Props>((
-  {
-    children,
-    showPopOver,
-    style,
-  },
-  ref,
-) => (
-  <AnimatePresence>
-    {
-      showPopOver && (
-        <motion.div
-          animate="enter"
-          className={styles.div}
-          exit="exit"
-          initial="initial"
-          ref={ref}
-          style={style}
-          variants={variants}
-        >
-          {children}
-        </motion.div>
-      )
+const PopOverAnimation: React.FC<Props> = ({
+  caretPlacement,
+  children,
+  placement,
+  popOverRef,
+  showPopOver,
+}) => {
+  const [popOverStyle, setPopOverStyle] = React.useState<PopOverStyle>(null);
+
+  React.useEffect(() => {
+    if (popOverRef.current) {
+      const style: PopOverStyle = getPopOverStyle(
+        popOverRef.current.clientHeight,
+        popOverRef.current.clientWidth,
+        placement,
+        !!caretPlacement,
+      );
+      setPopOverStyle(style);
     }
-  </AnimatePresence>
-));
+  }, [caretPlacement, placement, popOverRef]);
+
+  return (
+    <AnimatePresence>
+      {
+        showPopOver && (
+          <motion.div
+            animate="enter"
+            className={styles.div}
+            exit="exit"
+            initial="initial"
+            ref={popOverRef}
+            style={popOverStyle}
+            variants={variants}
+          >
+            {children}
+          </motion.div>
+        )
+      }
+    </AnimatePresence>
+  );
+};
 
 export default PopOverAnimation;
