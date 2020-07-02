3420f6049ddc748ac097483354bf01a85bdda9c4
Jimmy Emery <jemery@allcampus.com>
2020-07-02 16:54:22 -0400

    updating index

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,10 @@ import { ApolloProvider } from '@apollo/react-hooks';
 // @ts-ignore
 import { createRoot } from 'react-dom';
 import client from 'server/client';
-import PopOverDemo from 'modals/popOver/popOverStorybook/popOver.demo';
 import Spa from './spa/spa/spa';
 
-// createRoot(document.getElementById('app')).render(
-//   <ApolloProvider client={client}>
-//     <Spa />
-//   </ApolloProvider>,
-// );
-
 createRoot(document.getElementById('app')).render(
-  <div style={{ margin: '4rem', background: 'pink', width: '24rem' }}>
-    <PopOverDemo />
-  </div>,
+  <ApolloProvider client={client}>
+    <Spa />
+  </ApolloProvider>,
 );
