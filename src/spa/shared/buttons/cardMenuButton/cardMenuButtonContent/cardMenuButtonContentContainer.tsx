8d9a80079597cbfe8b84d6c36693810d3db66896
Jimmy Emery <jemery@allcampus.com>
2020-07-06 16:54:38 -0400

    updating

--- src/spa/shared/buttons/cardMenuButton/cardMenuButtonContent/cardMenuButtonContentContainer.tsx
+++ src/spa/shared/buttons/cardMenuButton/cardMenuButtonContent/cardMenuButtonContentContainer.tsx
@@ -11,8 +11,6 @@ const CardMenuButtonContentContainer: React.FC<Props> = ({ userCard }) => {
   const [deleteUserCard] = useMutation(DELETE_USER_CARD_MUTATION);
   const [toggleToDoUserCard] = useMutation(TOGGLE_TO_DO_USER_CARD_MUTATION);
   const [isToDo, setIsToDo] = React.useState<boolean>(userCard.isToDo);
-  const [showPopConfirm, setShowPopConfirm] = React.useState<boolean>(false);
-  const deleteButtonRef = React.useRef<HTMLDivElement>(null);
 
   const handleDelete = React.useCallback(async () => {
     await deleteUserCard({
@@ -34,11 +32,8 @@ const CardMenuButtonContentContainer: React.FC<Props> = ({ userCard }) => {
 
   return (
     <CardMenuButtonContentComponent
-      deleteButtonRef={deleteButtonRef}
       handleDelete={handleDelete}
       isToDo={isToDo}
-      setShowPopConfirm={setShowPopConfirm}
-      showPopConfirm={showPopConfirm}
       toggleToDo={toggleToDo}
     />
   );
