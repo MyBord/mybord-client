1ae3a3dbec7ac24f9c9085167f332f9612ae6faa
Jimmy <jimmy.e@outlook.com>
2020-07-06 16:40:44 -0400

    Card menu button update (#73)
    * updating
    
    * updating

--- src/spa/shared/cards/card/cardContent/cardContent.tsx
+++ src/spa/shared/cards/card/cardContent/cardContent.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useMutation } from '@apollo/react-hooks';
 import CardContentButtonsAnimation from 'framerMotion/cardContentButtonsAnimation';
-import CardMenuButtonContainer from 'buttons/cardMenuButton/cardMenuButtonContainer';
+import CardMenuButton from 'buttons/cardMenuButton/cardMenuButton';
 import FavoriteButton from 'buttons/favoriteButton/favoriteButton';
 import { TOGGLE_FAVORITE_USER_CARD_MUTATION, UserCard } from 'schema/card';
 import { useMultiSelectCardContext } from 'context/multiSelectCardContext/multiSelectCardContext';
@@ -16,7 +16,6 @@ const CardContent: React.FC<Props> = ({
   Content,
   userCard,
 }) => {
-  const containerRef = React.useRef(null);
   const [isFavorite, setIsFavorite] = React.useState<boolean>(userCard.isFavorite);
   const { activeCard, canMultiEdit } = useMultiSelectCardContext();
   const [toggleFavoriteUserCard] = useMutation(TOGGLE_FAVORITE_USER_CARD_MUTATION);
@@ -33,14 +32,14 @@ const CardContent: React.FC<Props> = ({
   const showButtons = !canMultiEdit && activeCard.id !== userCard.id;
 
   return (
-    <div className={styles.container} ref={containerRef}>
+    <div className={styles.container}>
       <CardContentButtonsAnimation showButtons={showButtons}>
         <FavoriteButton
           isFavorite={isFavorite}
           onClick={handleFavorite}
           size={25}
         />
-        <CardMenuButtonContainer containerRef={containerRef} userCard={userCard} />
+        <CardMenuButton userCard={userCard} />
       </CardContentButtonsAnimation>
       <Content />
     </div>
