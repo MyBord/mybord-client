59f38da4e0e0d793a0effcfd903456b8b7c70c05
Jimmy Emery <jemery@allcampus.com>
2020-07-07 20:00:07 -0400

    updating

--- src/spa/app/layout/header/headerTools/headerProfile/headerProfile.tsx
+++ src/spa/app/layout/header/headerTools/headerProfile/headerProfile.tsx
@@ -4,34 +4,15 @@ import PopOver from 'modals/popOver/popOver';
 import HeaderProfilePopOverContent from './headerProfilePopOverContent';
 import * as styles from './headerProfile.module.less';
 
-const HeaderProfile: React.FC = () => {
-  const popRef = React.useRef(null);
-
-  console.log('888');
-  console.log(popRef.current);
-  const bar = (): void => {
-    if (popRef.current) {
-      popRef.current.setShowPopOver(false);
-    }
-  };
-
-  const Content: React.FC = () => (
-    <>
-      <button type="button" onClick={bar}>Click Me</button>
-      <HeaderProfilePopOverContent />
-    </>
-  );
-
-  return (
-    <PopOver Content={<Content />} placement="bottom-right" ref={popRef}>
-      <div className={styles.div}>
-        <Icon iconName="avatar" size={40} />
-        <div className={styles.arrowDiv}>
-          <Icon iconName="downArrow" size={10} />
-        </div>
+const HeaderProfile: React.FC = () => (
+  <PopOver Content={<HeaderProfilePopOverContent />} placement="bottom-right">
+    <div className={styles.div}>
+      <Icon iconName="avatar" size={40} />
+      <div className={styles.arrowDiv}>
+        <Icon iconName="downArrow" size={10} />
       </div>
-    </PopOver>
-  );
-};
+    </div>
+  </PopOver>
+);
 
 export default HeaderProfile;
