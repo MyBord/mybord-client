af03bb83b04967cf21ed0fbd61d0e0957c67d943
Jimmy Emery <jemery@allcampus.com>
2020-07-12 11:54:42 -0400

    updating

--- src/spa/shared/cards/youtubeCard/youtubeCardDescription/youtubeCardDescription.tsx
+++ src/spa/shared/cards/youtubeCard/youtubeCardDescription/youtubeCardDescription.tsx
@@ -12,7 +12,7 @@ const YoutubeCardDescription: React.FC<Props> = ({ userCard }) => (
   <div className={styles.container}>
     <div className={styles.title}>
       <Typography
-        maxTextLength={30}
+        maxTextLength={60}
         size="two"
         text={userCard.cardData.youtubeCardData.videoTitle}
         weight="bold"
@@ -27,7 +27,7 @@ const YoutubeCardDescription: React.FC<Props> = ({ userCard }) => (
         />
         <div className={styles.channelTitle}>
           <Typography
-            maxTextLength={6}
+            maxTextLength={60}
             size="one"
             text={userCard.cardData.youtubeCardData.channelTitle}
           />
