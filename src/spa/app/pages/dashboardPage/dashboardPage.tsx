efbf2f495996cc30bb2415557d2e3e8f828e23ca
Jimmy Emery <jemery@allcampus.com>
2020-07-12 12:23:12 -0400

    adding dashboard page component

---
+++ src/spa/app/pages/dashboardPage/dashboardPage.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import DashboardPageContainer from './dashboardPageContainer';
+
+const DashboardPage: React.FC = () => <DashboardPageContainer />;
+
+export default DashboardPage;
