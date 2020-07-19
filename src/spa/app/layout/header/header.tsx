a5336a6f7213d5c1a8aa5618c97d682d358711de
Jimmy Emery <jemery@allcampus.com>
2020-07-19 13:25:31 -0400

    updating

--- src/spa/app/layout/header/header.tsx
+++ src/spa/app/layout/header/header.tsx
@@ -3,10 +3,31 @@ import HeaderProfile from './headerProfile/headerProfile';
 import PageHeader from './pageHeaders/pageHeader';
 import * as styles from './header.module.less';
 
+// const Header: React.FC = () => (
+//   <header className={styles.header}>
+//     <PageHeader />
+//     <HeaderProfile />
+//   </header>
+// );
+
 const Header: React.FC = () => (
-  <header className={styles.header}>
-    <PageHeader />
-    <HeaderProfile />
+  <header
+    className={styles.header}
+    style={{
+      border: '2px solid blue',
+    }}
+  >
+    <section
+      style={{
+        height: '3rem', // @headerHeight
+        background: 'red',
+        width: '100%',
+        top: '-1rem',
+        position: 'absolute',
+      }}
+    >
+      <h3 style={{ margin: '0' }}>This is the header</h3>
+    </section>
   </header>
 );
 
