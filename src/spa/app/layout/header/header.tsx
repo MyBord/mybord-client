1d4adb2e79b0d501006ae9c40fd60b23f2253ef6
Jimmy Emery <jemery@allcampus.com>
2020-07-19 14:09:32 -0400

    updating header animation

--- src/spa/app/layout/header/header.tsx
+++ src/spa/app/layout/header/header.tsx
@@ -4,43 +4,11 @@ import PageHeader from './pageHeaders/pageHeader';
 import HeaderAnimation from 'framerMotion/headerAnimation';
 import * as styles from './header.module.less';
 
-// const Header: React.FC = () => (
-//   <header className={styles.header}>
-//     <PageHeader />
-//     <HeaderProfile />
-//   </header>
-// );
-
-// const Header: React.FC = () => (
-//   <header
-//     className={styles.header}
-//     style={{
-//       border: '2px solid blue',
-//     }}
-//   >
-//     <section
-//       style={{
-//         height: '3rem', // @headerHeight
-//         background: 'red',
-//         width: '100%',
-//         top: '-1rem',
-//         position: 'absolute',
-//       }}
-//     >
-//       <h3 style={{ margin: '0' }}>This is the header</h3>
-//     </section>
-//   </header>
-// );
-
 const Header: React.FC = () => (
-  <header
-    className={styles.header}
-    style={{
-      border: '2px solid blue',
-    }}
-  >
+  <header className={styles.header}>
     <HeaderAnimation>
-      <h3 style={{ margin: '0' }}>This is the header</h3>
+      <PageHeader />
+      <HeaderProfile />
     </HeaderAnimation>
   </header>
 );
