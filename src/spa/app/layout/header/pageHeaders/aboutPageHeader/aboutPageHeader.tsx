a2338fedd504473c5e4968f11bfc11a8fe25dee8
Jimmy <jimmy.e@outlook.com>
2020-07-19 19:40:34 -0400

    adding about page header (#94)

---
+++ src/spa/app/layout/header/pageHeaders/aboutPageHeader/aboutPageHeader.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import Typography from 'typography/typography';
+import * as styles from './aboutPageHeader.module.less';
+
+const AboutPageHeader: React.FC = () => (
+  <section className={styles.section}>
+    <div>
+      <Typography
+        link="/about"
+        size="four"
+        text="About MyBord"
+      />
+    </div>
+    <div className={styles.devDiv}>
+      <Typography
+        link="/about"
+        size="four"
+        text="For Developers"
+      />
+    </div>
+  </section>
+
+);
+
+export default AboutPageHeader;
