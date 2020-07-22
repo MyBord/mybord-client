7871d375bb11307d7fb6b816e8b3342f6a5e0cc0
Jimmy Emery <jemery@allcampus.com>
2020-07-22 15:55:37 -0400

    updating

--- src/spa/app/pages/aboutDevPage/aboutDevPage.tsx
+++ src/spa/app/pages/aboutDevPage/aboutDevPage.tsx
@@ -9,16 +9,23 @@ const AboutDevPage: React.FC = () => (
   <section className={styles.parentSection}>
     <section className={styles.section}>
       <section className={styles.paragraphSection}>
-        <div className={styles.paragraphDiv}>
+        <div className={styles.paragraphDiv} id="p-1">
           <Typography text={loremIpsumLong} textAlign="justify" />
         </div>
         <img alt="laptop" className={styles.img} src={laptop} />
       </section>
-      <section className={styles.paragraphSection}>
+      <section className={styles.paragraphSection} id="p-2">
         <img alt="laptop" className={styles.img} src={laptop} />
         <div className={styles.paragraphDiv}>
           <Typography text={loremIpsumLong} textAlign="justify" />
         </div>
+      </section>
+      <section className={styles.stackSection}>
+        <Typography color="blue" size="five" text="Stack" />
+        <Typography color="blue" size="four" text="General" />
+        <Typography color="blue" size="four" text="Front End" />
+        <Typography color="blue" size="four" text="Back End" />
+        <div style={{ height: '30rem', width: '30rem', background: 'pink' }} />
       </section>
     </section>
   </section>
