ec84a43366ad1314715787b2305b986f19287f96
Jimmy Emery <jemery@allcampus.com>
2020-07-22 15:01:40 -0400

    updating

--- src/spa/app/pages/aboutDevPage/aboutDevPage.tsx
+++ src/spa/app/pages/aboutDevPage/aboutDevPage.tsx
@@ -6,14 +6,14 @@ import { loremIpsumLong } from 'utils/loremIpsum';
 import * as styles from './aboutDevPage.module.less';
 
 const AboutDevPage: React.FC = () => (
-  <section className={styles.section}>
-    <section className={styles.paragraphSection}>
-      <div className={styles.paragraphDiv}>
-        <Typography text={loremIpsumLong} />
-      </div>
-      <div>
+  <section className={styles.parentSection}>
+    <section className={styles.section}>
+      <section className={styles.paragraphSection}>
+        <div className={styles.paragraphDiv}>
+          <Typography text={loremIpsumLong} />
+        </div>
         <img alt="laptop" className={styles.img} src={laptop} />
-      </div>
+      </section>
     </section>
   </section>
 );
