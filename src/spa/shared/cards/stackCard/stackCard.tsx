4ddfd99de594ea54900afdeffeefe64eb1142228
Jimmy Emery <jemery@allcampus.com>
2020-07-23 14:54:34 -0400

    adding stack card config

--- src/spa/shared/cards/stackCard/stackCard.tsx
+++ src/spa/shared/cards/stackCard/stackCard.tsx
@@ -1,122 +1,14 @@
 import * as React from 'react';
-import antDesign from 'assets/icons/antDesign.png';
-import apollo from 'assets/icons/apollo.png';
-import babel from 'assets/icons/babel.png';
-import circleCi from 'assets/icons/circleCi.png';
-import cssModules from 'assets/icons/cssModules.png';
-import docker from 'assets/icons/docker.png';
-import enzyme from 'assets/icons/enzyme.png';
-import eslint from 'assets/icons/eslint.png';
-import express from 'assets/icons/express.png';
-import framerMotion from 'assets/icons/framerMotion.png';
-import graphql from 'assets/icons/graphql.png';
-import heroku from 'assets/icons/heroku.png';
-import jest from 'assets/icons/jest.png';
-import less from 'assets/icons/less.png';
-import lottie from 'assets/icons/lottie.png';
-import node from 'assets/icons/node.png';
-import nucleo from 'assets/icons/nucleo.png';
-import passport from 'assets/icons/passport.png';
-import postgresql from 'assets/icons/postgresql.png';
-import prisma from 'assets/icons/prisma.png';
-import react from 'assets/icons/react.png';
-import storybook from 'assets/icons/storybook.png';
-import typescript from 'assets/icons/typescript.png';
-import webpack from 'assets/icons/webpack.png';
+import config, { Id } from './stackCardConfig';
 import * as styles from './stackCard.module.less';
 
-const getImage = (id: Props['id']): string => {
-  switch (id) {
-    case 'antDesign':
-      return antDesign;
-    case 'apolloClient':
-      return apollo;
-    case 'apolloServer':
-      return apollo;
-    case 'babel':
-      return babel;
-    case 'circleCi':
-      return circleCi;
-    case 'cssModules':
-      return cssModules;
-    case 'docker':
-      return docker;
-    case 'enzyme':
-      return enzyme;
-    case 'eslint':
-      return eslint;
-    case 'express':
-      return express;
-    case 'framerMotion':
-      return framerMotion;
-    case 'graphql':
-      return graphql;
-    case 'heroku':
-      return heroku;
-    case 'jest':
-      return jest;
-    case 'less':
-      return less;
-    case 'lottie':
-      return lottie;
-    case 'node':
-      return node;
-    case 'nucleo':
-      return nucleo;
-    case 'passport':
-      return passport;
-    case 'postgresql':
-      return postgresql;
-    case 'prisma':
-      return prisma;
-    case 'prismaCloud':
-      return prisma;
-    case 'react':
-      return react;
-    case 'storybook':
-      return storybook;
-    case 'typescript':
-      return typescript;
-    case 'webpack':
-      return webpack;
-    default:
-      throw Error(`Unknown id in stack card: ${id}`);
-  }
-};
-
-interface Props {
-  id:
-  'antDesign' |
-  'apolloClient' |
-  'apolloServer' |
-  'babel' |
-  'circleCi' |
-  'cssModules' |
-  'docker' |
-  'enzyme' |
-  'eslint' |
-  'express' |
-  'framerMotion' |
-  'graphql' |
-  'heroku' |
-  'jest' |
-  'less' |
-  'lottie' |
-  'node' |
-  'nucleo' |
-  'passport' |
-  'postgresql' |
-  'prisma' |
-  'prismaCloud' |
-  'react' |
-  'storybook' |
-  'typescript' |
-  'webpack';
+export interface Props {
+  id: Id;
 }
 
 const StackCard: React.FC<Props> = ({ id }) => (
   <div className={styles.div}>
-    <img alt="react" className={styles.img} src={getImage(id)} />
+    <img alt="react" className={styles.img} src={config[id].png} />
   </div>
 );
 
---
+++ src/spa/shared/cards/stackCard/stackCardConfig.ts
@@ -0,0 +1,89 @@
+import antDesign from 'assets/icons/antDesign.png';
+import apollo from 'assets/icons/apollo.png';
+import babel from 'assets/icons/babel.png';
+import circleCi from 'assets/icons/circleCi.png';
+import cssModules from 'assets/icons/cssModules.png';
+import docker from 'assets/icons/docker.png';
+import enzyme from 'assets/icons/enzyme.png';
+import eslint from 'assets/icons/eslint.png';
+import express from 'assets/icons/express.png';
+import framerMotion from 'assets/icons/framerMotion.png';
+import graphql from 'assets/icons/graphql.png';
+import heroku from 'assets/icons/heroku.png';
+import jest from 'assets/icons/jest.png';
+import less from 'assets/icons/less.png';
+import lottie from 'assets/icons/lottie.png';
+import node from 'assets/icons/node.png';
+import nucleo from 'assets/icons/nucleo.png';
+import passport from 'assets/icons/passport.png';
+import postgresql from 'assets/icons/postgresql.png';
+import prisma from 'assets/icons/prisma.png';
+import react from 'assets/icons/react.png';
+import storybook from 'assets/icons/storybook.png';
+import typescript from 'assets/icons/typescript.png';
+import webpack from 'assets/icons/webpack.png';
+
+export type Id =
+'antDesign' |
+'apolloClient' |
+'apolloServer' |
+'babel' |
+'circleCi' |
+'cssModules' |
+'docker' |
+'enzyme' |
+'eslint' |
+'express' |
+'framerMotion' |
+'graphql' |
+'heroku' |
+'jest' |
+'less' |
+'lottie' |
+'node' |
+'nucleo' |
+'passport' |
+'postgresql' |
+'prisma' |
+'prismaCloud' |
+'react' |
+'storybook' |
+'typescript' |
+'webpack';
+
+type Config = {
+  [key in Id]: {
+    png: string;
+  };
+};
+
+const config: Config = {
+  antDesign: { png: antDesign },
+  apolloClient: { png: apollo },
+  apolloServer: { png: apollo },
+  babel: { png: babel },
+  circleCi: { png: circleCi },
+  cssModules: { png: cssModules },
+  docker: { png: docker },
+  enzyme: { png: enzyme },
+  eslint: { png: eslint },
+  express: { png: express },
+  framerMotion: { png: framerMotion },
+  graphql: { png: graphql },
+  heroku: { png: heroku },
+  jest: { png: jest },
+  less: { png: less },
+  lottie: { png: lottie },
+  node: { png: node },
+  nucleo: { png: nucleo },
+  passport: { png: passport },
+  postgresql: { png: postgresql },
+  prisma: { png: prisma },
+  prismaCloud: { png: prisma },
+  react: { png: react },
+  storybook: { png: storybook },
+  typescript: { png: typescript },
+  webpack: { png: webpack },
+};
+
+export default config;
