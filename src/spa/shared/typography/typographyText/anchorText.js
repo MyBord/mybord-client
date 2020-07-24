94b15de0b4024de19b2ad49db01b5eba19bd8ebe
Jimmy Emery <jemery@allcampus.com>
2020-07-24 17:41:51 -0400

    updating anchor text

--- src/spa/shared/typography/typographyText/anchorText.js
+++ src/spa/shared/typography/typographyText/anchorText.js
@@ -1,29 +1,34 @@
-const sampleText = ' foo bar baz [hello](google.com) [again] (foo.com)';
+const sampleText = ' foo bar baz [hello](google.com) [again] (foo.com)  abc  ttt [w](x) ';
 
-const getBracketIndices = (text) => {
-  const bracketIndices = [];
-  let bracketIndex = 0;
-  let str = text;
+const getLeftBracketPositions = (text) => {
+  const regex = RegExp('\\[', 'gi');
+  const indices = [];
 
-  do {
-    let remainingString;
-
-    if (bracketIndex === 0) {
-      remainingString = str;
-    } else {
-      remainingString = str.substring(bracketIndex + 1);
-    }
-
-    bracketIndex = remainingString.indexOf('[');
-    str = remainingString;
-
-    if (bracketIndex !== -1) {
-      bracketIndices.push(bracketIndex);
-    }
+  while (regex.exec(text)) {
+    indices.push(regex.lastIndex);
   }
-  while (bracketIndex !== -1);
 
-  return bracketIndices;
-}
+  return indices;
+};
 
-console.log(getBracketIndices(sampleText));
+const fn = (text) => {
+  const leftBracketPositions = getLeftBracketPositions(text);
+
+  leftBracketPositions.forEach((leftBracket) => {
+    const remainingString = text.substring(leftBracket);
+    const rightBracket = remainingString.indexOf(']');
+    const leftParen = remainingString.indexOf('(');
+    const rightParen = remainingString.indexOf(')');
+
+    if (
+      rightBracket < leftParen
+      && rightBracket < rightParen
+      && leftParen < rightParen
+      && rightBracket === leftParen - 1
+    ) {
+      console.log(remainingString);
+    }
+  });
+};
+
+fn(sampleText);
