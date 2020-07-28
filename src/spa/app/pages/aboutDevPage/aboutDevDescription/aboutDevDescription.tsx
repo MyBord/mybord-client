15e776652832c529f5790a79766e307bb159692f
Jimmy Emery <jemery@allcampus.com>
2020-07-28 17:06:11 -0400

    updating about dev page styling

--- src/spa/app/pages/aboutDevPage/aboutDevDescription/aboutDevDescription.tsx
+++ src/spa/app/pages/aboutDevPage/aboutDevDescription/aboutDevDescription.tsx
@@ -8,16 +8,16 @@ import * as styles from './aboutDevDescription.module.less';
 const AboutDevDescription: React.FC = () => (
   <>
     <section className={styles.paragraphSection}>
+      <img alt="programming" className={styles.img} src={programming} />
       <div className={styles.paragraphDiv} id="p-1">
         <Typography size="four" text={partOne} textAlign="justify" />
       </div>
-      <img alt="programming" className={styles.img} src={programming} />
     </section>
     <section className={styles.paragraphSection} id="p-2">
-      <img alt="programming" className={styles.img} src={programming} />
       <div className={styles.paragraphDiv}>
         <Typography size="four" text={loremIpsumLong} textAlign="justify" />
       </div>
+      <img alt="programming" className={styles.img} src={programming} />
     </section>
   </>
 );
