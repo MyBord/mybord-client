852cb3ef247ca7946e51e890b6455ac9d2744184
Jimmy Emery <jemery@allcampus.com>
2020-07-28 13:38:36 -0400

    updating fallback

--- src/spa/spa/spa.tsx
+++ src/spa/spa/spa.tsx
@@ -3,10 +3,22 @@ import { BrowserRouter } from 'react-router-dom';
 import SpaProviders from './spaProviders';
 const App = React.lazy(() => import('app/app'));
 
+const Fallback: React.FC = () => (
+  <div
+    style={{
+      background: 'red',
+      height: '30rem',
+      width: '30rem',
+    }}
+  >
+    LOADING
+  </div>
+);
+
 const Spa: React.FC = () => (
   <BrowserRouter>
     <SpaProviders>
-      <React.Suspense fallback={<div>loading</div>}>
+      <React.Suspense fallback={<Fallback />}>
         <App />
       </React.Suspense>
     </SpaProviders>
