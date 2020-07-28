f3af174fa5e1498dc2ba91edb605185582f37fbc
Jimmy Emery <jemery@allcampus.com>
2020-07-28 14:00:17 -0400

    adding about dev stack

---
+++ src/spa/app/pages/aboutDevPage/aboutDevStack/aboutDevStack.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import StackCard from 'cards/stackCard/stackCard';
+import Typography from 'typography/typography';
+import * as styles from './aboutDevStack.module.less';
+
+const AboutDevStack: React.FC = () => (
+  <section className={styles.stackSection}>
+    <div className={styles.stackTitleDiv}>
+      <Typography color="blue" size="five" text="Stack" />
+    </div>
+    <div className={styles.typographyDiv}>
+      <Typography color="blue" size="four" text="General" />
+    </div>
+    <div className={styles.stackCardsContainer}>
+      <StackCard id="typescript" />
+      <StackCard id="graphql" />
+      <StackCard id="webpack" />
+      <StackCard id="babel" />
+      <StackCard id="eslint" />
+      <StackCard id="circleCi" />
+      <StackCard id="jest" />
+    </div>
+    <div className={styles.typographyDiv}>
+      <Typography color="blue" size="four" text="Front End" />
+    </div>
+    <div className={styles.stackCardsContainer}>
+      <StackCard id="react" />
+      <StackCard id="apolloClient" />
+      <StackCard id="framerMotion" />
+      <StackCard id="storybook" />
+      <StackCard id="lottie" />
+      <StackCard id="nucleo" />
+      <StackCard id="less" />
+      <StackCard id="cssModules" />
+      <StackCard id="enzyme" />
+      <StackCard id="antDesign" />
+    </div>
+    <div className={styles.typographyDiv}>
+      <Typography color="blue" size="four" text="Back End" />
+    </div>
+    <div className={styles.stackCardsContainer}>
+      <StackCard id="node" />
+      <StackCard id="apolloServer" />
+      <StackCard id="prisma" />
+      <StackCard id="express" />
+      <StackCard id="postgresql" />
+      <StackCard id="passport" />
+    </div>
+    <div className={styles.typographyDiv}>
+      <Typography color="blue" size="four" text="Deployment & Hosting" />
+    </div>
+    <div className={styles.stackCardsContainer}>
+      <StackCard id="heroku" />
+      <StackCard id="prismaCloud" />
+      <StackCard id="docker" />
+    </div>
+  </section>
+);
+
+export default AboutDevStack;
