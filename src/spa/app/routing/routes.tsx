144a8ba48896430a053a6259e2a7dd7ceb1697f1
Jimmy Emery <jemery@allcampus.com>
2020-07-28 12:01:07 -0400

    updating

--- src/spa/app/routing/routes.tsx
+++ src/spa/app/routing/routes.tsx
@@ -1,23 +1,22 @@
 import * as React from 'react';
-import { Switch, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 const AboutPage = React.lazy(() => import('pages/aboutPage/aboutPage'));
 const DashboardPage = React.lazy(() => import('pages/dashboardPage/dashboardPage'));
 const TrendingPage = React.lazy(() => import('pages/trendingPage/trendingPage'));
-import { MotionRoute } from 'utils/routing';
 
 const Routes: React.FC = () => {
   const location = useLocation();
   return (
     <Switch location={location} key={location.key}>
-      <MotionRoute exact path="/about">
+      <Route exact path="/about">
         <AboutPage />
-      </MotionRoute>
-      <MotionRoute exact path="/myBord">
+      </Route>
+      <Route exact path="/myBord">
         <DashboardPage />
-      </MotionRoute>
-      <MotionRoute exact path="/trending">
+      </Route>
+      <Route exact path="/trending">
         <TrendingPage />
-      </MotionRoute>
+      </Route>
     </Switch>
   );
 };
