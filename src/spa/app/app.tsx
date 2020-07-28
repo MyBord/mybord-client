a62147fd53ad0280830edf30dbe38509b9c74702
Jimmy Emery <jemery@allcampus.com>
2020-07-28 12:49:13 -0400

    updating

--- src/spa/app/app.tsx
+++ src/spa/app/app.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import ErrorBoundary from 'app/errorBoundary/errorBoundary';
 import Layout from 'layout/layout';
 import Routes from 'routing/routes';
 
 const App: React.FC = () => (
-  <Layout>
-    <ErrorBoundary>
-      <Routes />
-    </ErrorBoundary>
-  </Layout>
+  <div>
+    <Link to="/myBord">MyBord</Link>
+    <Link to="/about">About</Link>
+    <Link to="/trending">Trending</Link>
+    <Layout>
+      <ErrorBoundary>
+        <Routes />
+      </ErrorBoundary>
+    </Layout>
+  </div>
 );
 
 export default App;
