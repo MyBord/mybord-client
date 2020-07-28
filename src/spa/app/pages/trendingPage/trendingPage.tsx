f734b7365e44295f3993bf2eeb20e2eeca5e54fb
Jimmy Emery <jemery@allcampus.com>
2020-07-28 13:08:13 -0400

    updating

--- src/spa/app/pages/trendingPage/trendingPage.tsx
+++ src/spa/app/pages/trendingPage/trendingPage.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import LottiePlayer from 'lotty/lottiePlayer';
 import pageWrapper from 'pages/pageWrapper/pageWrapper';
-import trendingAnimation from 'lotty/lotties/trending.json';
 
 const TrendingPage: React.FC = () => (
-  <LottiePlayer
-    animationData={trendingAnimation}
-    autoplay
-    loop
-    size={600}
-  />
+  <div
+    style={{
+      background: 'pink',
+      height: '10rem',
+      width: '10rem',
+    }}
+  >
+    Trending Page
+  </div>
 );
 
 export default pageWrapper({ Component: TrendingPage });
