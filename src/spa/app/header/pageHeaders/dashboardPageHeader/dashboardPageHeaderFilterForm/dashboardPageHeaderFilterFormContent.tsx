d561b0e14d4b9b3ebd7c1a7678c8fe3157f043c5
Jimmy Emery <jemery@allcampus.com>
2020-07-29 14:47:26 -0400

    updating header filter form

--- src/spa/app/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterFormContent.tsx
+++ src/spa/app/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterFormContent.tsx
@@ -5,7 +5,6 @@ import IconButton from 'icons/iconButton/iconButton';
 import PopOver from 'modals/popOver/popOver';
 import TextInput from 'inputs/textInput/textInput';
 import { FormProp } from 'types/formTypes';
-import { useMultiSelectCardContext } from 'context/multiSelectCardContext/multiSelectCardContext';
 import DashboardPageHeaderFiltersContent
   from './dashboardPageHeaderFiltersContent/dashboardPageHeaderFiltersContent';
 import * as styles from './dashboardPageHeaderFilterForm.module.less';
@@ -20,53 +19,29 @@ const DashboardPageHeaderFilterFormContent: React.FC<Props> = ({
   errorMessage,
   form,
   isWaiting,
-}) => {
-  const { canMultiEdit, toggleMultiEditStatus } = useMultiSelectCardContext();
-
-  return (
-    <div className={styles.formContainer}>
-      <FormItem
-        errorMessage={errorMessage}
-        fieldName="add-youtube-video-input"
-        form={form}
-        required
-        requiredMessage="A url is required"
-      >
-        <TextInput overlayClassName={styles.input} placeholder="youtube url" />
-      </FormItem>
-      <div className={styles.buttonsContainer}>
-        <FormItem fieldName="add-youtube-video-submit" form={form}>
-          <div className={styles.buttonDiv}>
-            <Button
-              htmlType="submit"
-              isWaiting={isWaiting}
-              label="Add Video"
-            />
-          </div>
-        </FormItem>
-      </div>
-      <div className={styles.buttonsContainer}>
-        <PopOver
-          Content={<DashboardPageHeaderFiltersContent />}
-          caretPlacement="center"
-          placement="bottom-center"
-        >
-          <IconButton
-            color="blue"
-            iconName="filter"
-            size={32}
+}) => (
+  <div className={styles.formContainer}>
+    <FormItem
+      errorMessage={errorMessage}
+      fieldName="add-youtube-video-input"
+      form={form}
+      required
+      requiredMessage="A url is required"
+    >
+      <TextInput overlayClassName={styles.input} placeholder="youtube url" />
+    </FormItem>
+    <div className={styles.buttonsContainer}>
+      <FormItem fieldName="add-youtube-video-submit" form={form}>
+        <div className={styles.buttonDiv}>
+          <Button
+            htmlType="submit"
+            isWaiting={isWaiting}
+            label="Add Video"
           />
-        </PopOver>
-      </div>
-      {/* <FormItem fieldName="edit-cards" form={form}> */}
-      {/*  <Button */}
-      {/*    htmlType="button" */}
-      {/*    label={canMultiEdit ? 'Editing' : 'Edit'} */}
-      {/*    onClick={toggleMultiEditStatus} */}
-      {/*  /> */}
-      {/* </FormItem> */}
+        </div>
+      </FormItem>
     </div>
-  );
-};
+  </div>
+);
 
 export default DashboardPageHeaderFilterFormContent;
