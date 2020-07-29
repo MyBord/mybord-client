96af72d1877780e59c87e53f82fb5e43f86cb635
Jimmy Emery <jemery@allcampus.com>
2020-07-29 13:59:30 -0400

    removing location value and hook from routes.tsx

--- src/spa/app/routes.tsx
+++ src/spa/app/routes.tsx
@@ -1,24 +1,21 @@
 import * as React from 'react';
-import { Route, Switch, useLocation } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 const AboutPage = React.lazy(() => import('./aboutPage'));
 const PageOne = React.lazy(() => import('./pageOne'));
 const TrendingPage = React.lazy(() => import('./trendingPage'));
 
-const Routes: React.FC = () => {
-  const location = useLocation();
-  return (
-    <Switch location={location} key={location.key}>
-      <Route exact path="/pageOne">
-        <PageOne />
-      </Route>
-      <Route exact path="/pageTwo">
-        <AboutPage />
-      </Route>
-      <Route exact path="/pageThree">
-        <TrendingPage />
-      </Route>
-    </Switch>
-  );
-};
+const Routes: React.FC = () => (
+  <Switch>
+    <Route exact path="/pageOne">
+      <PageOne />
+    </Route>
+    <Route exact path="/pageTwo">
+      <AboutPage />
+    </Route>
+    <Route exact path="/pageThree">
+      <TrendingPage />
+    </Route>
+  </Switch>
+);
 
 export default Routes;
