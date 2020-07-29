eeac7cffc987c094f1dfffa73649deb8204c112d
Jimmy Emery <jemery@allcampus.com>
2020-07-29 16:30:37 -0400

    updating about dev page to include description

--- src/spa/landing/pages/aboutDevPage/aboutDevPage.tsx
+++ src/spa/landing/pages/aboutDevPage/aboutDevPage.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import AboutDevDescription from 'pages/aboutDevPage/aboutDevDescription/aboutDevDescription';
+import AboutDevStack from 'pages/aboutDevPage/aboutDevStack/aboutDevStack';
 import pageWrapper from 'landing/pages/pageWrapper/pageWrapper';
 import * as styles from './aboutDevPage.module.less';
 
-const AboutDevPage: React.FC = () => <div className={styles.div}>About Dev Page</div>;
+const AboutDevPage: React.FC = () => (
+  <section className={styles.section}>
+    <AboutDevDescription />
+    <AboutDevStack />
+  </section>
+);
 
 export default pageWrapper(AboutDevPage);
--- src/spa/landing/pages/aboutPage/aboutPage.tsx
+++ src/spa/landing/pages/aboutPage/aboutPage.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 import pageWrapper from 'landing/pages/pageWrapper/pageWrapper';
 import * as styles from './aboutPage.module.less';
 
-const AboutPage: React.FC = () => <div className={styles.div}>About</div>;
+const AboutPage: React.FC = () => <section className={styles.section}>About</section>;
 
 export default pageWrapper(AboutPage);
