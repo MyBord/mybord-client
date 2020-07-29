db743c34042873102021c5fb6f142e66c54ad2e1
Jimmy Emery <jemery@allcampus.com>
2020-07-29 13:33:36 -0400

    updating

--- src/spa/app/pages/dashboardPage/dashboardPageContainer.tsx
+++ src/spa/app/pages/dashboardPage/dashboardPageContainer.tsx
@@ -1,22 +1,6 @@
 import * as React from 'react';
 import pageWrapper from 'pages/pageWrapper';
-import { useSubscription } from '@apollo/react-hooks';
-import {
-  DELETED_USER_CARD_SUBSCRIPTION,
-  FILTERED_USER_CARDS_SUBSCRIPTION,
-  USER_CARD_SUBSCRIPTION,
-  USER_CARDS_QUERY,
-  UserCard,
-} from 'schema/card';
-import {
-  ADD_CARD,
-  DELETE_CARD,
-  RECEIVE_FILTERED_CARDS,
-  SET_CARDS,
-} from 'context/dashboardCardsContext/dashboardCardsReducerTypes';
-import { useDashboardCardsContext } from 'context/dashboardCardsContext/dashboardCardsContext';
-import { useHydrationContext } from 'context/hydrationContext/hydrationContext';
-import DashboardPageComponent from './dashboardPageComponent';
+import { USER_CARDS_QUERY, UserCard } from 'schema/card';
 
 interface Props {
   data: {
@@ -25,76 +9,24 @@ interface Props {
 }
 
 const DashboardPageContainer: React.FC<Props> = ({ data }) => {
-  const { state, dispatch } = useDashboardCardsContext();
-  const { setHydrationStatus } = useHydrationContext();
+  console.log(' ----- DASHBOARD DATA ----- ');
+  console.log(data);
+  console.log(' ----- DASHBOARD DATA ----- ');
 
-  // ----- ADDING CARDS ----- //
-  // first adding cards to state that were initially retrieved when our page was loaded
-
-  React.useEffect(() => {
-    dispatch({ type: SET_CARDS, cards: data.userCards });
-  }, [dispatch, data.userCards]);
-
-  // ----- ADDING A CARD ----- //
-  // subscribing to when a card gets added
-
-  const { data: cardData, loading: cardLoading } = useSubscription(USER_CARD_SUBSCRIPTION);
-
-  React.useEffect(() => {
-    if (!cardLoading) {
-      dispatch({ type: ADD_CARD, card: cardData.userCard });
-    }
-  }, [cardData, cardLoading, dispatch]);
-
-  // ----- DELETING A CARD ----- //
-  // subscribing to when a card gets deleted
-
-  const {
-    data: deleteData,
-    loading: deleteLoading,
-  } = useSubscription(DELETED_USER_CARD_SUBSCRIPTION);
-
-  React.useEffect(() => {
-    if (!deleteLoading) {
-      dispatch({ type: DELETE_CARD, id: deleteData.deletedUserCard.id });
-    }
-  }, [dispatch, deleteData, deleteLoading]);
-
-  // ----- SUBSCRIBING TO FILTERS ----- //
-  // subscribing to when cards get filtered
-
-  const {
-    data: filteredUserCardsData,
-    loading: filteredUserCardsLoading,
-  } = useSubscription(FILTERED_USER_CARDS_SUBSCRIPTION);
-
-  React.useEffect(() => {
-    if (!filteredUserCardsLoading) {
-      dispatch({
-        type: RECEIVE_FILTERED_CARDS,
-        cards: filteredUserCardsData.filteredUserCards.userCards,
-        filters: filteredUserCardsData.filteredUserCards.filters,
-      });
-    }
-  }, [filteredUserCardsData, filteredUserCardsLoading, dispatch]);
-
-  // ----- TURNS OFF LOADING ANIMATION ----- //
-  // this turns off the loading animation / spinner for the page once our reducer has been
-  // properly hydrated
-
-  React.useEffect(() => {
-    if (state.isHydrated) {
-      setHydrationStatus(true);
-    }
-  }, [setHydrationStatus, state.isHydrated]);
-
-  // ----- RETURNING THE CHILD COMPONENT ----- //
-
-  if (state.isHydrated) {
-    return <DashboardPageComponent />;
-  }
-
-  return null;
+  return (
+    <div
+      style={{
+        alignItems: 'center',
+        background: 'blue',
+        display: 'flex',
+        height: '15rem',
+        justifyContent: 'center',
+        width: '15rem',
+      }}
+    >
+      <h1 style={{ color: 'white' }}>Dashboard Page</h1>
+    </div>
+  );
 };
 
 export default pageWrapper({
