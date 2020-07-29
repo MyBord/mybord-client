532412899a9f7b1558ee568f25a2c25b9d527155
Jimmy Emery <jemery@allcampus.com>
2020-07-29 13:53:33 -0400

    removing header animation

--- src/spa/app/header/header.tsx
+++ src/spa/app/header/header.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react';
 import HeaderProfile from './headerProfile/headerProfile';
 import PageHeader from './pageHeaders/pageHeader';
-import HeaderAnimation from 'framerMotion/headerAnimation';
 import * as styles from './header.module.less';
 
 const Header: React.FC = () => (
   <header className={styles.header}>
-    <HeaderAnimation>
-      <PageHeader />
-      <HeaderProfile />
-    </HeaderAnimation>
+    <PageHeader />
+    <HeaderProfile />
   </header>
 );
 
