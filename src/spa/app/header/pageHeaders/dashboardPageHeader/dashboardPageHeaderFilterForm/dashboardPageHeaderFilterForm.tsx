b18e81733133f266ed382fdf69d1eeef3f0d2df6
Jimmy Emery <jemery@allcampus.com>
2020-07-29 14:50:35 -0400

    updating header form

--- src/spa/app/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterForm.tsx
+++ src/spa/app/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterForm.tsx
@@ -1,22 +1,11 @@
 import * as React from 'react';
-import { useMutation } from '@apollo/react-hooks';
 import Form from 'forms/form/form';
-import { CREATE_YOUTUBE_CARD_MUTATION } from 'schema/card';
 import DashboardPageHeaderFilterFormContent from './dashboardPageHeaderFilterFormContent';
 
-const DashboardPageHeaderFilterForm: React.FC = () => {
-  const [createYoutubeCard] = useMutation(CREATE_YOUTUBE_CARD_MUTATION);
-  const [inputErrorMessage, setInputErrorMessage] = React.useState<string>(null);
-  const [isSubmitWaiting, setIsSubmitWaiting] = React.useState<boolean>(false);
-
-  return (
-    <Form onSubmit={() => {}}>
-      <DashboardPageHeaderFilterFormContent
-        errorMessage={inputErrorMessage}
-        isWaiting={isSubmitWaiting}
-      />
-    </Form>
-  );
-};
+const DashboardPageHeaderFilterForm: React.FC = () => (
+  <Form onSubmit={() => {}}>
+    <DashboardPageHeaderFilterFormContent />
+  </Form>
+);
 
 export default DashboardPageHeaderFilterForm;
--- src/spa/app/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterFormContent.tsx
+++ src/spa/app/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterFormContent.tsx
@@ -1,45 +1,21 @@
 import * as React from 'react';
 import Button from 'buttons/button/button';
 import FormItem from 'forms/formItem/formItem';
-import IconButton from 'icons/iconButton/iconButton';
-import PopOver from 'modals/popOver/popOver';
 import TextInput from 'inputs/textInput/textInput';
 import { FormProp } from 'types/formTypes';
-import DashboardPageHeaderFiltersContent
-  from './dashboardPageHeaderFiltersContent/dashboardPageHeaderFiltersContent';
 import * as styles from './dashboardPageHeaderFilterForm.module.less';
 
 interface Props {
-  errorMessage: string;
   form?: FormProp;
-  isWaiting: boolean;
 }
 
-const DashboardPageHeaderFilterFormContent: React.FC<Props> = ({
-  errorMessage,
-  form,
-  isWaiting,
-}) => (
+const DashboardPageHeaderFilterFormContent: React.FC<Props> = ({ form }) => (
   <div className={styles.formContainer}>
-    <FormItem
-      errorMessage={errorMessage}
-      fieldName="add-youtube-video-input"
-      form={form}
-      required
-      requiredMessage="A url is required"
-    >
-      <TextInput overlayClassName={styles.input} placeholder="youtube url" />
-    </FormItem>
+    <TextInput overlayClassName={styles.input} placeholder="youtube url" />
     <div className={styles.buttonsContainer}>
-      <FormItem fieldName="add-youtube-video-submit" form={form}>
-        <div className={styles.buttonDiv}>
-          <Button
-            htmlType="submit"
-            isWaiting={isWaiting}
-            label="Add Video"
-          />
-        </div>
-      </FormItem>
+      <div className={styles.buttonDiv}>
+        <Button htmlType="submit" label="Add Video" />
+      </div>
     </div>
   </div>
 );
