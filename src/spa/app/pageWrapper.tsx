b3954f796ac9405f56fd068e72a89ebb041431ee
Jimmy Emery <jemery@allcampus.com>
2020-07-29 14:19:07 -0400

    updating how a no data page is handled in the pageWrapper

--- src/spa/app/pageWrapper.tsx
+++ src/spa/app/pageWrapper.tsx
@@ -1,13 +1,3 @@
-// This objective of this wrapper is to create a general api that takes a page of our app
-// along with the GQL query that derives all necessary data for said page and runs that query.
-// The data is then provided to the page to render its necessary components.
-
-// when `setHydration=false`, that means that once our data has been retrieved from the backend
-// and / or our wrapped component (`Component`) has mounted, we DO NOT want to stop our page's
-// spinner / loading animation. This is because there might be additional work that is *still*
-// happening in the background of the wrapped component after data has been drilled, such as a
-// context reducer being mutated.
-
 import * as React from 'react';
 import api from 'api/api';
 import { useHydrationContext } from 'context/hydrationContext/hydrationContext';
@@ -29,18 +19,7 @@ const pageWrapper = ({
 }: Props): React.FC => {
   // ----- NO DATA IS NEEDED ----- //
   if (!gqlString) {
-    const NoDataPage: React.FC = () => {
-      const { setHydrationStatus } = useHydrationContext();
-
-      // See *1 in `hydrationContext.tsx`
-      React.useEffect(() => {
-        if (setHydration) {
-          setHydrationStatus(true);
-        }
-      }, [setHydrationStatus]);
-
-      return <Component />;
-    };
+    const NoDataPage: React.FC = () => <Component />;
 
     return NoDataPage;
   }
