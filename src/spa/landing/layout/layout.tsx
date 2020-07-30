53b34db3bb55609cb5202b1e889a6d010d1a128a
Jimmy Emery <jemery@allcampus.com>
2020-07-30 15:47:48 -0400

    updating landing layout

--- src/spa/landing/layout/layout.tsx
+++ src/spa/landing/layout/layout.tsx
@@ -7,13 +7,23 @@ interface Props {
 }
 
 const Layout: React.FC<Props> = ({ children }) => (
-  <main>
-    <img
-      alt="login-form-background"
-      className={styles.img}
-      src={loginBackground}
-    />
+  <main className={styles.main}>
+    <div className={styles.imgContainer}>
+      <img
+        alt="login-form-background"
+        className={styles.img}
+        src={loginBackground}
+      />
+    </div>
     {children}
+    <section className={styles.section}><h1>Section 1</h1></section>
+    <section className={styles.section}><h1>Section 2</h1></section>
+    <section className={styles.section}><h1>Section 3</h1></section>
+    <section className={styles.section}><h1>Section 4</h1></section>
+    <section className={styles.section}><h1>Section 5</h1></section>
+    <section className={styles.section}><h1>Section 6</h1></section>
+    <section className={styles.section}><h1>Section 7</h1></section>
+    <section className={styles.section}><h1>Section 8</h1></section>
   </main>
 );
 
