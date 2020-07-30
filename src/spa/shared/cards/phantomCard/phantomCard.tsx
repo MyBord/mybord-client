a50557ea8133f4f5ebf2d41fb648dc54a80d4fc0
Jimmy Emery <jemery@allcampus.com>
2020-07-29 21:01:29 -0400

    removing shared components

--- src/spa/shared/cards/phantomCard/phantomCard.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-import * as styles from './phantomCard.module.less';
-
-const PhantomCard: React.FC = () => <div className={styles.div} />;
-
-export default PhantomCard;
