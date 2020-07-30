721fc1296dcbd57be81053cced6f536283a9ba2a
Jimmy Emery <jemery@allcampus.com>
2020-07-29 21:10:07 -0400

    removing app folder in spa folder

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import 'babel-polyfill';
 import * as React from 'react';
 // @ts-ignore
 import { createRoot } from 'react-dom';
-import Spa from './spa/app/spa';
+import Spa from './spa/spa';
 
 createRoot(document.getElementById('app')).render(<Spa />);
--- src/spa/spa.tsx
+++ src/spa/spa.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import App from '../app/app';
+import App from './app';
 
 const Fallback: React.FC = () => (
   <div
