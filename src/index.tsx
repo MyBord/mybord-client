dda4f7dcc689a3b801b29534761cd2dcb8137718
Jimmy Emery <jemery@allcampus.com>
2020-07-29 21:08:46 -0400

    removing apollo client from index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,7 @@
 import 'babel-polyfill';
 import * as React from 'react';
-import { ApolloProvider } from '@apollo/react-hooks';
 // @ts-ignore
 import { createRoot } from 'react-dom';
-import client from 'server/client';
 import Spa from './spa/app/spa';
 
-createRoot(document.getElementById('app')).render(
-  <ApolloProvider client={client}>
-    <Spa />
-  </ApolloProvider>,
-);
+createRoot(document.getElementById('app')).render(<Spa />);
