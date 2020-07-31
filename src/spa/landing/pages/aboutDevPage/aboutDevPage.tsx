15e2dcaebb206e762a67dca35af12dda5305f210
Jimmy Emery <jemery@allcampus.com>
2020-07-30 20:44:39 -0400

    updating about pages for landing

--- src/spa/landing/pages/aboutDevPage/aboutDevPage.tsx
+++ src/spa/landing/pages/aboutDevPage/aboutDevPage.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react';
 import AboutDevDescription from 'pages/aboutDevPage/aboutDevDescription/aboutDevDescription';
 import AboutDevStack from 'pages/aboutDevPage/aboutDevStack/aboutDevStack';
-import AboutPageHeader from 'header/pageHeaders/aboutPageHeader/aboutPageHeader';
 import pageWrapper from 'landing/pages/pageWrapper/pageWrapper';
-import * as styles from './aboutDevPage.module.less';
 
 const AboutDevPage: React.FC = () => (
-  <section className={styles.section}>
-    <div className={styles.divHeader}>
-      <AboutPageHeader />
-    </div>
+  <>
     <AboutDevDescription />
     <AboutDevStack />
-  </section>
+  </>
 );
 
 export default pageWrapper(AboutDevPage);
--- src/spa/landing/pages/aboutPage/aboutPage.tsx
+++ src/spa/landing/pages/aboutPage/aboutPage.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import pageWrapper from 'landing/pages/pageWrapper/pageWrapper';
-import * as styles from './aboutPage.module.less';
 
-const AboutPage: React.FC = () => <section className={styles.section}>About</section>;
+const AboutPage: React.FC = () => <h1>About</h1>;
 
 export default pageWrapper(AboutPage);
