c5aef15de8637682f60a6fd1693713f85ff004a6
Jimmy Emery <jemery@allcampus.com>
2020-08-04 16:39:02 -0400

    removing utils folder

--- src/utils/cssUtils.ts
+++
@@ -1,5 +0,0 @@
-// get the numerical unit from a given string, e.g. "24px" => 24
-export const getUnit = (string: string, unit: string): number => Number(string.split(unit)[0]);
-
-// converts numerical value to a string unit, e.g. 24 => "24px"
-export const pixelize = (value: number, unit: string): string => `${value}${unit}`;
