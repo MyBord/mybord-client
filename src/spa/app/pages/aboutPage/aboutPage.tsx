150507a6b19f336543692ec43eaa1c0a2cb606dd
Jimmy Emery <jemery@allcampus.com>
2020-08-06 11:41:13 -0400

    updating about page

--- src/spa/app/pages/aboutPage/aboutPage.tsx
+++ src/spa/app/pages/aboutPage/aboutPage.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import pageWrapper from 'pages/pageWrapper/pageWrapper';
+import AboutDescription from './aboutDescription/aboutDescription';
+import * as styles from './aboutPage.module.less';
 
-const AboutPage: React.FC = () => <h1>This is the about page</h1>;
+const AboutPage: React.FC = () => (
+  <section className={styles.parentSection}>
+    <section className={styles.section}>
+      <AboutDescription />
+    </section>
+  </section>
+);
 
 export default pageWrapper({ Component: AboutPage });
