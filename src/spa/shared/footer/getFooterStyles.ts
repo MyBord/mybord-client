c017310bec800e8ea0246dde002a9a545e2f9059
Jimmy Emery <jemery@allcampus.com>
2020-08-07 12:38:42 -0400

    updating

--- src/spa/shared/footer/getFooterStyles.ts
+++ src/spa/shared/footer/getFooterStyles.ts
@@ -1,22 +1,24 @@
 import * as React from 'react';
-import { FooterHrStyle, FooterImgStyle } from 'types/footerTypes';
-import { makeUnit } from 'utils/cssUtils';
 import * as sizes from 'styles/_sizes.less';
+import { FooterHrStyle, FooterImgStyle } from 'types/footerTypes';
+import { convertUnit, getUnit, makeUnit } from 'utils/cssUtils';
 
-// make 96 an export
 export const getHrStyles = (ref: React.RefObject<HTMLDivElement>): FooterHrStyle => {
+  const listMargin = convertUnit(sizes.footerListMargin, 'rem', 'px');
+  const footerMargin = getUnit(listMargin, 'px');
   const rect = ref.current.getBoundingClientRect();
-  const width = makeUnit(rect.width - 96, 'px');
+  const width = makeUnit(rect.width - (footerMargin * 2), 'px');
   return {
     width,
   };
 };
 
 // don't always be in center if it is wider
-// make 16 an export
 export const getImgStyles = (ref: React.RefObject<HTMLDivElement>): FooterImgStyle => {
+  const logoSize = convertUnit(sizes.footerLogoSize, 'rem', 'px');
+  const logoWidthOffset = getUnit(logoSize, 'px') / 2;
   const rect = ref.current.getBoundingClientRect();
-  const left = makeUnit(rect.left / 2 - 16, 'px');
+  const left = makeUnit(rect.left / 2 - logoWidthOffset, 'px');
   return {
     left,
   };
--- src/utils/cssUtils.ts
+++ src/utils/cssUtils.ts
@@ -1,5 +1,23 @@
+type Unit = 'px' | 'rem';
+
 // get the numerical unit from a given string, e.g. "24px" => 24
-export const getUnit = (string: string, unit: string): number => Number(string.split(unit)[0]);
+export const getUnit = (string: string, unit: Unit): number => Number(string.split(unit)[0]);
 
 // converts numerical value to a string unit, e.g. 24 => "24px"
-export const makeUnit = (value: number, unit: string): string => `${value}${unit}`;
+export const makeUnit = (value: number, unit: Unit): string => `${value}${unit}`;
+
+// converts one string css unit to another
+export const convertUnit = (string: string, currentUnit: Unit, nextUnit: Unit): string => {
+  const clonedString = `${string}`;
+  const value = Number(clonedString.replace(currentUnit, ''));
+
+  if (currentUnit === 'rem' && nextUnit === 'px') {
+    return makeUnit(value * 16, 'px');
+  }
+
+  if (currentUnit === 'px' && nextUnit === 'rem') {
+    return makeUnit(value / 16, 'rem');
+  }
+
+  throw Error('Could not convert unit');
+};
