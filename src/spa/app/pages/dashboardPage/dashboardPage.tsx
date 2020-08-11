4fc25f6e8bf82fc5bf9a224002114b73f9dda8fa
Jimmy Emery <jemery@allcampus.com>
2020-08-10 20:47:21 -0400

    udpating

--- src/spa/app/pages/dashboardPage/dashboardPage.tsx
+++ src/spa/app/pages/dashboardPage/dashboardPage.tsx
@@ -1,6 +1,12 @@
 import * as React from 'react';
+import AddCardModal from 'modals/addCardModal/addCardModal';
 import DashboardPageContainer from './dashboardPageContainer';
 
-const DashboardPage: React.FC = () => <DashboardPageContainer />;
+const DashboardPage: React.FC = () => (
+  <>
+    <AddCardModal />
+    <DashboardPageContainer />
+  </>
+);
 
 export default DashboardPage;
