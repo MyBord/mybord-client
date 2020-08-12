9c72cb7d2ee56340022f7f4507731c1f553371c1
Jimmy Emery <jemery@allcampus.com>
2020-08-12 09:15:22 -0400

    updating

--- src/spa/shared/modals/modal/modalFooter/modalFooter.tsx
+++ src/spa/shared/modals/modal/modalFooter/modalFooter.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
+import Anchor from 'inputs/anchor/anchor';
 import Button from 'buttons/button/button';
 import * as styles from './modalFooter.module.less';
 
 const ModalFooter: React.FC = () => (
-  <>
-    <Button label="add" />
+  <div className={styles.div}>
     <Button label="add" type="secondary" />
-  </>
+    <Anchor label="foo" />
+    <Button label="add" />
+  </div>
 );
 
 export default ModalFooter;
