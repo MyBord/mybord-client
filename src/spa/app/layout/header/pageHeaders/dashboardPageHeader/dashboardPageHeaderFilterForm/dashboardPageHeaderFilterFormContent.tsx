e7f099f2a43f5c6b7f8665d34acbe52760f39122
Jimmy Emery <jemery@allcampus.com>
2020-08-13 12:02:57 -0400

    updating

--- src/spa/app/layout/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterFormContent.tsx
+++ src/spa/app/layout/header/pageHeaders/dashboardPageHeader/dashboardPageHeaderFilterForm/dashboardPageHeaderFilterFormContent.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import AddCardModal from 'modals/addCardModal/addCardModal';
 import Button from 'buttons/button/button';
 import FormItem from 'forms/formItem/formItem';
 import IconButton from 'icons/iconButton/iconButton';
@@ -24,48 +25,51 @@ const DashboardPageHeaderFilterFormContent: React.FC<Props> = ({
   const { canMultiEdit, toggleMultiEditStatus } = useMultiSelectCardContext();
 
   return (
-    <div className={styles.formContainer}>
-      <FormItem
-        errorMessage={errorMessage}
-        fieldName="add-youtube-video-input"
-        form={form}
-        required
-        requiredMessage="A url is required"
-      >
-        <TextInput overlayClassName={styles.input} placeholder="youtube url" />
-      </FormItem>
-      <div className={styles.buttonsContainer}>
-        <FormItem fieldName="add-youtube-video-submit" form={form}>
-          <div className={styles.buttonDiv}>
-            <Button
-              htmlType="submit"
-              isWaiting={isWaiting}
-              label="Add Video"
-            />
-          </div>
-        </FormItem>
-      </div>
-      <div className={styles.buttonsContainer}>
-        <PopOver
-          Content={<DashboardPageHeaderFiltersContent />}
-          caretPlacement="center"
-          placement="bottom-center"
+    <>
+      <AddCardModal />
+      <div className={styles.formContainer}>
+        <FormItem
+          errorMessage={errorMessage}
+          fieldName="add-youtube-video-input"
+          form={form}
+          required
+          requiredMessage="A url is required"
         >
-          <IconButton
-            color="blue"
-            iconName="filter"
-            size={32}
-          />
-        </PopOver>
+          <TextInput overlayClassName={styles.input} placeholder="youtube url" />
+        </FormItem>
+        <div className={styles.buttonsContainer}>
+          <FormItem fieldName="add-youtube-video-submit" form={form}>
+            <div className={styles.buttonDiv}>
+              <Button
+                htmlType="submit"
+                isWaiting={isWaiting}
+                label="Add Video"
+              />
+            </div>
+          </FormItem>
+        </div>
+        <div className={styles.buttonsContainer}>
+          <PopOver
+            Content={<DashboardPageHeaderFiltersContent />}
+            caretPlacement="center"
+            placement="bottom-center"
+          >
+            <IconButton
+              color="blue"
+              iconName="filter"
+              size={32}
+            />
+          </PopOver>
+        </div>
+        {/* <FormItem fieldName="edit-cards" form={form}> */}
+        {/*  <Button */}
+        {/*    htmlType="button" */}
+        {/*    label={canMultiEdit ? 'Editing' : 'Edit'} */}
+        {/*    onClick={toggleMultiEditStatus} */}
+        {/*  /> */}
+        {/* </FormItem> */}
       </div>
-      {/* <FormItem fieldName="edit-cards" form={form}> */}
-      {/*  <Button */}
-      {/*    htmlType="button" */}
-      {/*    label={canMultiEdit ? 'Editing' : 'Edit'} */}
-      {/*    onClick={toggleMultiEditStatus} */}
-      {/*  /> */}
-      {/* </FormItem> */}
-    </div>
+    </>
   );
 };
 
--- src/spa/shared/modals/addCardModal/addCardModal.tsx
+++ src/spa/shared/modals/addCardModal/addCardModal.tsx
@@ -1,19 +1,33 @@
 import * as React from 'react';
 import Modal from 'modals/modal/modal';
+import { ModalCallback } from 'types/modalTypes';
 import AddCardModalForm from './addCardModalForm/addCardModalForm';
 import * as styles from './addCardModal.module.less';
 
-const AddCardModal: React.FC = () => (
-  <Modal defaultVisible title="Add To My Bord">
-    <div className={styles.parentDiv}>
-      <div className={styles.previewDiv}>
-        foo bar
-      </div>
-      <div className={styles.formDiv}>
-        <AddCardModalForm />
-      </div>
-    </div>
-  </Modal>
-);
+const AddCardModal: React.FC = () => {
+  const [hideModal, setHideModal] = React.useState<ModalCallback['hideModal']>(null);
+  const [showModal, setShowModal] = React.useState<ModalCallback['showModal']>(null);
+
+  const callback = (props: ModalCallback): void => {
+    setHideModal(props.hideModal);
+    setShowModal(props.showModal);
+  };
+
+  return (
+    <>
+      <button type="button" onClick={showModal}>click me</button>
+      <Modal callback={callback} title="Add To My Bord">
+        <div className={styles.parentDiv}>
+          <div className={styles.previewDiv}>
+            foo bar
+          </div>
+          <div className={styles.formDiv}>
+            <AddCardModalForm />
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
+};
 
 export default AddCardModal;
