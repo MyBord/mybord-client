bdb120958ce558997001da1ab437b87a8951075f
Jimmy Emery <jimmy.e@outlook.com>
2020-10-13 21:30:31 -0400

    udpating

--- src/spa/app/pages/userSettingsPage/userSettingsPageForm/deleteAccountModal/deleteAccountModal.tsx
+++ src/spa/app/pages/userSettingsPage/userSettingsPageForm/deleteAccountModal/deleteAccountModal.tsx
@@ -5,28 +5,50 @@ import FormItem from 'forms/formItem/formItem';
 import Modal from 'modals/modal/modal';
 import TextInput from 'inputs/textInput/textInput';
 import Typography from 'typography/typography';
+import { FormProp } from 'types/formTypes';
 import * as styles from './deleteAccountModal.module.less';
 
-const DeleteAccountModal: React.FC = () => (
-  <Modal id="user-settings-delete-account-modal" title="Delete Account">
-    <div className={styles.container}>
-      <Form>
+interface FormContentProps {
+  form?: FormProp;
+}
+
+const DeleteAccountModal: React.FC = () => {
+  const [canDelete, setCanDelete] = React.useState<boolean>(false);
+  const FormContent: React.FC<FormContentProps> = ({ form }) => {
+    const fieldName = 'delete-account-input-confirm';
+    const fieldValue: string = form.getFieldValue(fieldName);
+
+    React.useEffect(() => {
+      if (fieldValue && fieldValue.toLowerCase() === 'delete') {
+        setCanDelete(true);
+      } else {
+        setCanDelete(false);
+      }
+    }, [fieldValue]);
+
+    return (
+      <FormItem fieldName={fieldName} form={form}>
+        <TextInput color="red" />
+      </FormItem>
+    );
+  };
+
+  return (
+    <Modal id="user-settings-delete-account-modal" title="Delete Account">
+      <div className={styles.container}>
         <div className={styles.typographyContainer}>
           <Typography color="red" text="Are you sure you want to delete your account?" />
           <Typography color="red" text={'Please type "delete" to confirm:'} />
         </div>
-        <FormItem fieldName="delete-account-input-confirm">
-          <TextInput />
-        </FormItem>
-        <FormItem fieldName="elete-account-input-confirm">
-          <TextInput color="red" />
-        </FormItem>
-      </Form>
-      <div className={styles.buttonDiv}>
-        <Button color="red" label="Delete My Account" />
+        <Form>
+          <FormContent />
+        </Form>
+        <div className={styles.buttonDiv}>
+          <Button color="red" label="Delete My Account" />
+        </div>
       </div>
-    </div>
-  </Modal>
-);
+    </Modal>
+  );
+};
 
 export default DeleteAccountModal;
