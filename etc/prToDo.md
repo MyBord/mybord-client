a819392f19617a608d7e17ba4ff0d09905d61e9d
Jimmy Emery <jimmy.e@outlook.com>
2020-10-13 20:58:07 -0400

    updating

--- etc/prToDo.md
+++ etc/prToDo.md
@@ -1,3 +1,5 @@
 # NOTE: This is a space to make todo's for the pr / branch that you are currently working on. 
 Make sure to remove all notes before merging (except for this one, of course).
 ----------------------------------------------------------------------------------------------------
+hover for input should be red
+text for input should be red
--- src/spa/app/pages/userSettingsPage/userSettingsPageForm/deleteAccountModal/deleteAccountModal.tsx
+++ src/spa/app/pages/userSettingsPage/userSettingsPageForm/deleteAccountModal/deleteAccountModal.tsx
@@ -1,9 +1,28 @@
 import * as React from 'react';
+import Button from 'buttons/button/button';
+import Form from 'forms/form/form';
+import FormItem from 'forms/formItem/formItem';
 import Modal from 'modals/modal/modal';
+import TextInput from 'inputs/textInput/textInput';
+import Typography from 'typography/typography';
+import * as styles from './deleteAccountModal.module.less';
 
 const DeleteAccountModal: React.FC = () => (
   <Modal id="user-settings-delete-account-modal" title="Delete Account">
-    <h1>hello world</h1>
+    <div className={styles.container}>
+      <Form>
+        <div className={styles.typographyContainer}>
+          <Typography color="red" text="Are you sure you want to delete your account?" />
+          <Typography color="red" text={'Please type "delete" to confirm:'} />
+        </div>
+        <FormItem fieldName="delete-account-input-confirm">
+          <TextInput />
+        </FormItem>
+      </Form>
+      <div className={styles.buttonDiv}>
+        <Button color="red" label="Delete My Account" />
+      </div>
+    </div>
   </Modal>
 );
 
