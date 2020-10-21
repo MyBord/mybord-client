3f5ecaf3b14b39374517d988dcb8e2254344933e
Jimmy Emery <jimmy.e@outlook.com>
2020-10-21 16:57:14 -0400

    adding debugging

--- src/spa/app/pages/userDashboardPage/userDashboardPage.tsx
+++ src/spa/app/pages/userDashboardPage/userDashboardPage.tsx
@@ -1,6 +1,38 @@
 import * as React from 'react';
+import Portal from 'portal/portal';
 import UserDashboardPageContainer from './userDashboardPageContainer';
+import PopOver from 'modals/popOver/popOver';
 
-const UserDashboardPage: React.FC = () => <UserDashboardPageContainer />;
+const Content: React.FC = () => {
+  console.log('lll');
+  return (
+    <>
+      <h1>hello world</h1>
+      <Portal>
+        <div
+          style={{
+            background: 'pink',
+            width: '4rem',
+            height: '4rem',
+            position: 'absolute',
+            left: 0,
+            top: 0,
+          }}
+        >
+          foo bar
+        </div>
+      </Portal>
+    </>
+  );
+};
+
+const UserDashboardPage: React.FC = () => (
+  <>
+    <PopOver Content={<Content />}>
+      <h1>Click Me</h1>
+    </PopOver>
+    <UserDashboardPageContainer />
+  </>
+);
 
 export default UserDashboardPage;
