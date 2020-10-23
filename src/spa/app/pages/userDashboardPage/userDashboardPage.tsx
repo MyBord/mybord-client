254d96bcc4d3a835875465aee9d77887e4de88b7
Jimmy Emery <jimmy.e@outlook.com>
2020-10-23 19:56:38 -0400

    updating

--- src/spa/app/pages/userDashboardPage/userDashboardPage.tsx
+++ src/spa/app/pages/userDashboardPage/userDashboardPage.tsx
@@ -1,34 +1,6 @@
 import * as React from 'react';
-import Dropdown from 'inputs/dropdown/dropdown';
-import { mockDropdownOptions } from 'mockData/inputsMockData';
-import PopOver from 'modals/popOver/popOver';
-import { ContentPopOverProps } from 'types/modalTypes';
 import UserDashboardPageContainer from './userDashboardPageContainer';
 
-const Content: React.FC<ContentPopOverProps> = ({ setChildRefs }) => {
-  const sampleRef = React.useRef(null);
-
-  React.useEffect(() => {
-    if (sampleRef && sampleRef.current) {
-      setChildRefs([sampleRef]);
-    }
-  }, [sampleRef]);
-
-  return (
-    <div ref={sampleRef}>
-      <h1>hello world</h1>
-      <Dropdown options={mockDropdownOptions} />
-    </div>
-  );
-};
-
-const UserDashboardPage: React.FC = () => (
-  <>
-    <PopOver Content={<Content />}>
-      <h1>Click Me</h1>
-    </PopOver>
-    <UserDashboardPageContainer />
-  </>
-);
+const UserDashboardPage: React.FC = () => <UserDashboardPageContainer />;
 
 export default UserDashboardPage;
