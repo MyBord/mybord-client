f4e1a9291d490d3f75b3790270473fdb500eee05
Jimmy Emery <jimmy.e@outlook.com>
2020-11-03 21:10:05 -0500

    updating names

--- src/spa/app/layout/header/pageHeaders/userDashboardPageHeader/userDashboardPageHeader.tsx
+++ src/spa/app/layout/header/pageHeaders/userDashboardPageHeader/userDashboardPageHeader.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import AddCardModal from 'modals/addCardModal/addCardModal';
-import UserDashboardPageHeaderFilterForm
-  from './userDashboardPageHeaderFilterForm/userDashboardPageHeaderFilterForm';
+import UserDashboardPageHeaderForm from './userDashboardPageHeaderForm/userDashboardPageHeaderForm';
 import * as styles from './userDashboardPageHeader.module.less';
 
 const UserDashboardPageHeader: React.FC = () => (
   <>
     <AddCardModal />
     <section className={styles.section}>
-      <UserDashboardPageHeaderFilterForm />
+      <UserDashboardPageHeaderForm />
     </section>
   </>
 );
