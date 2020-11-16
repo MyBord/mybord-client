31ca8f9825b08ab06d402659e23b0040f92688e9
Jimmy <jimmy.e@outlook.com>
2020-11-16 12:13:18 -0500

    Adding Favicon (#159)
    * updating index to add favicon
    
    * updating line

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { ApolloProvider } from '@apollo/react-hooks';
 // @ts-ignore
 import { createRoot } from 'react-dom';
 import client from 'server/client';
+// This import is included just to make sure that webpack will include the favicon in it's build
+// / bundle process, so that index.html can have reference to it later.
+import myBordMark from 'assets/logo/myBordMark.png';
 import Spa from './spa/spa/spa';
 
 createRoot(document.getElementById('app')).render(
