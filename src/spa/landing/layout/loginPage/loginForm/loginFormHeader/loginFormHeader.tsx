f283fdf7100831646868eb5242ffe6be673989df
Jimmy Emery <jimmy.e@outlook.com>
2020-11-16 11:49:17 -0500

    updating

--- src/spa/landing/layout/loginPage/loginForm/loginFormHeader/loginFormHeader.tsx
+++ src/spa/landing/layout/loginPage/loginForm/loginFormHeader/loginFormHeader.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
-import logo from 'assets/logo/myBordMark.png';
 import logoWithType from 'assets/logo/myBordMarkType.png';
-import Typography from 'typography/typography';
 import * as styles from './loginFormHeader.module.less';
 
 const LoginFormHeader: React.FC = () => (
   <div className={styles.div}>
     <img alt="MyBord logo" className={styles.img} src={logoWithType} />
-    {/* <Typography size="five" text="MyBord.io" weight="bold" /> */}
   </div>
 );
 
