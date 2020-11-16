71ce2dbd0929b29cacccc7d90a2ff7682225ac56
Jimmy Emery <jimmy.e@outlook.com>
2020-11-16 11:45:25 -0500

    udpating

--- src/spa/landing/layout/loginPage/loginForm/loginFormHeader/loginFormHeader.tsx
+++ src/spa/landing/layout/loginPage/loginForm/loginFormHeader/loginFormHeader.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-import logo from 'assets/logo/mb.png';
+import logo from 'assets/logo/myBordMark.png';
+import logoWithType from 'assets/logo/myBordMarkType.png';
 import Typography from 'typography/typography';
 import * as styles from './loginFormHeader.module.less';
 
 const LoginFormHeader: React.FC = () => (
   <div className={styles.div}>
-    <img alt="MyBord logo" className={styles.img} src={logo} />
-    <Typography size="five" text="MyBord.io" weight="bold" />
+    <img alt="MyBord logo" className={styles.img} src={logoWithType} />
+    {/* <Typography size="five" text="MyBord.io" weight="bold" /> */}
   </div>
 );
 
