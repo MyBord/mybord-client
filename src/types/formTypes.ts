42668ea5f6f68c97947c6359e92fce8df62a7917
Jimmy Emery <jimmy.e@outlook.com>
2020-11-26 18:25:10 -0500

    adding custom validation

--- src/types/formTypes.ts
+++ src/types/formTypes.ts
@@ -1,7 +1,11 @@
-import { FormComponentProps, GetFieldDecoratorOptions } from 'antd/es/form/Form';
+import { FormComponentProps, GetFieldDecoratorOptions, ValidationRule } from 'antd/es/form/Form';
 
 export type FormProps = FormComponentProps;
 
 export type FormProp = FormComponentProps['form'];
 
 export type GetFieldDecoratorOptionsType = GetFieldDecoratorOptions;
+
+export type Validator = ValidationRule['validator'];
+
+export type ValidationRules = ValidationRule[];
