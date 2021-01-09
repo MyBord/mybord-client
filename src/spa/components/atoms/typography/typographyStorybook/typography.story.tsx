3b448c8ba9289f171016f2f217f33337103f6581
Jimmy Emery <jimmy.e@outlook.com>
2021-01-09 12:21:17 -0500

    updating

--- src/spa/components/atoms/typography/typographyStorybook/typography.story.tsx
+++ src/spa/components/atoms/typography/typographyStorybook/typography.story.tsx
@@ -10,6 +10,12 @@ const TypographyStory: React.FC = () => (
     <Typography size="h2" text="Lorem ipsum dolor sit amet" />
     <Typography size="h3" text="h3" />
     <Typography size="h3" text="Lorem ipsum dolor sit amet" />
+    <Typography size="h3" text="h4" />
+    <Typography size="h4" text="Lorem ipsum dolor sit amet" />
+    <Typography size="h3" text="h5" />
+    <Typography size="h5" text="Lorem ipsum dolor sit amet" />
+    <Typography size="h3" text="h6" />
+    <Typography size="h6" text="Lorem ipsum dolor sit amet" />
   </>
 );
 
