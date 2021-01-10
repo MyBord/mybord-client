b3b583522112fd0d3f3c0e5661dd8830d0c4a50c
Jimmy Emery <jimmy.e@outlook.com>
2021-01-10 16:48:36 -0500

    updating

--- src/spa/components/atoms/inputs/checkbox/checkbox.tsx
+++ src/spa/components/atoms/inputs/checkbox/checkbox.tsx
@@ -14,34 +14,38 @@ const Checkbox: React.FC<Props> = ({
 }) => (
   <div
     aria-checked={checked}
-    className={[
-      styles.styledCheckbox,
-      checked ? styles.checked : styles.unchecked,
-      disabled ? styles[`disabled-${checked ? 'checked' : 'unchecked'}`] : undefined,
-    ].join(' ')}
     aria-disabled={disabled}
+    className={styles.divContainer}
     onClick={onClick}
     onKeyPress={onClick}
     role="checkbox"
     tabIndex={0}
   >
-    <svg
+    <div
       className={[
-        styles.svg,
-        checked ? styles.svgVisible : styles.svgHidden,
+        styles.checkbox,
+        checked ? styles.checked : styles.unchecked,
+        disabled ? styles[`disabled-${checked ? 'checked' : 'unchecked'}`] : undefined,
       ].join(' ')}
-      fill="none"
-      height="11"
-      viewBox="0 0 13 11"
-      width="13"
     >
-      <path
-        d="M2 6.27778L4.73913 9L11 2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="3"
-      />
-    </svg>
+      <svg
+        className={[
+          styles.svg,
+          checked ? styles.svgVisible : styles.svgHidden,
+        ].join(' ')}
+        fill="none"
+        height="11"
+        viewBox="0 0 13 11"
+        width="13"
+      >
+        <path
+          d="M2 6.27778L4.73913 9L11 2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="3"
+        />
+      </svg>
+    </div>
   </div>
 );
 
