9bc92a97e11f0faf77c88a7d978fda481c82684d
Jimmy Emery <jimmy.e@outlook.com>
2021-01-10 20:23:22 -0500

    updating text input

--- src/spa/components/atoms/inputs/textInput/textInput.tsx
+++ src/spa/components/atoms/inputs/textInput/textInput.tsx
@@ -2,14 +2,21 @@ import * as React from 'react';
 import * as styles from './textInput.module.less';
 
 interface Props {
+  disabled?: boolean;
   placeholder?: string;
 }
 
 const TextInput: React.FC<Props> = ({
+  disabled = false,
   placeholder = null,
 }) => (
   <input
-    className={styles.textInput}
+    className={[
+      styles.textInput,
+      disabled ? styles.disabled : undefined,
+    ].join(' ')}
+    disabled={disabled}
+    placeholder={placeholder}
     type="text"
   />
 );
--- src/spa/components/atoms/inputs/textInput/textInputStorybook/textInput.story.tsx
+++ src/spa/components/atoms/inputs/textInput/textInputStorybook/textInput.story.tsx
@@ -5,7 +5,15 @@ import TextInput from '../textInput';
 const TextInputStory: React.FC = () => (
   <>
     <Typography size="h1" text="Standard Text Input" />
+    <Typography size="h3" text="Default" />
     <TextInput />
+    <Typography size="h3" text="w/ Placeholder" />
+    <TextInput placeholder="lorem ipsum dolor sit amet" />
+    <Typography size="h1" text="Disabled Text Input" />
+    <Typography size="h3" text="Default" />
+    <TextInput disabled />
+    <Typography size="h3" text="w/ Placeholder" />
+    <TextInput disabled placeholder="lorem ipsum dolor sit amet" />
   </>
 );
 
