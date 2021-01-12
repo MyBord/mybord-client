33dedd91eb518cd22c4172d00675b2f725feec38
Jimmy Emery <jimmy.e@outlook.com>
2021-01-11 19:23:42 -0500

    updating

--- src/spa/components/atoms/popOver/popOverStorybook/popOver.story.tsx
+++ src/spa/components/atoms/popOver/popOverStorybook/popOver.story.tsx
@@ -3,24 +3,17 @@ import Typography from 'typography/typography';
 import PopOver from '../popOver';
 import * as styles from './popOverStory.module.less';
 
-const PopOverBox: React.FC = () => (
-  <div className={styles.div}>
+const PopOverBox = React.forwardRef<HTMLDivElement, {}>(({}, ref) => (
+  <div ref={ref} className={styles.div}>
     <Typography color="white" size="bodyDefault" text="Click Me" />
   </div>
-);
+));
 
 const PopOverStory: React.FC = () => (
   <>
     <Typography size="h1" text="Standard PopOver" />
-    <Typography size="h3" text="w/ React FC" />
     <PopOver Content={<h1>hello world</h1>}>
       <PopOverBox />
-    </PopOver>
-    <Typography size="h3" text="w/out React FC" />
-    <PopOver Content={<h1>hello world</h1>}>
-      <div className={styles.div}>
-        <Typography color="white" size="bodyDefault" text="Click Me" />
-      </div>
     </PopOver>
   </>
 );
