20ca86d650e90985afcdb7292e24cd1b093190ff
Jimmy Emery <jimmy.e@outlook.com>
2021-01-14 12:21:53 -0500

    updating

--- src/spa/components/molecules/modals/popLinkMenu/popLinkMenu.tsx
+++ src/spa/components/molecules/modals/popLinkMenu/popLinkMenu.tsx
@@ -1,5 +1,27 @@
 import * as React from 'react';
+import PopOver from 'popOver/popOver';
+import { PopOverProps } from 'types/modalTypes';
+import PopLinkMenuContent from './popLinkMenuContent';
 
-const PopLinkMenu: React.FC = () => <h1>hello world</h1>;
+interface Props {
+  children: PopOverProps['children'];
+  options: {
+    label: string;
+    link: string;
+  }[];
+}
+
+const PopLinkMenu: React.FC<Props> = ({
+  children,
+  options,
+}) => (
+  <PopOver
+    Content={<PopLinkMenuContent options={options} />}
+    caretPlacement="center"
+    placement="right-bottom"
+  >
+    {children}
+  </PopOver>
+);
 
 export default PopLinkMenu;
--- src/spa/components/molecules/modals/popLinkMenu/popLinkMenuStorybook/popLinkMenu.story.tsx
+++ src/spa/components/molecules/modals/popLinkMenu/popLinkMenuStorybook/popLinkMenu.story.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import Typography from 'typography/typography';
+import { PopOverBox } from 'storybook/commonStorybookComponents';
 import PopLinkMenu from '../popLinkMenu';
+
+const linkOptions = [
+  { label: 'My Settings', link: 'foo' },
+  { label: 'About MyBord', link: 'bar' },
+  { label: 'Logout', link: 'baz' },
+];
 
 const PopLinkMenuStory: React.FC = () => (
   <>
     <Typography text="Standard PopLinkMenu" type="h1" />
-    <PopLinkMenu />
+    <PopLinkMenu options={linkOptions}>
+      <PopOverBox />
+    </PopLinkMenu>
   </>
 );
 
