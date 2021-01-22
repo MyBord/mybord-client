334221b674cd9bec71c140d1e719c5061bc8ac69
Jimmy Emery <jimmy.e@outlook.com>
2021-01-21 19:48:32 -0500

    upadting

--- src/mockData/userCardMockData.ts
+++ src/mockData/userCardMockData.ts
@@ -1,13 +1,41 @@
 import { UserCardData } from 'schema/card';
 
-export const defaultUserCardMockData: UserCardData = {
+const defaultData: UserCardData = {
   id: '1',
   cardData: null,
   category: 'Video',
   isFavorite: false,
   isToDo: false,
   subtitle: 'Augue at ut arcu risus euismod morbi blandit non facilisi.',
-  tags: ['lorem', 'ipsum', 'consectetur', 'dolor sit', 'adipiscing', 'elit'],
+  tags: ['lorem', 'dolor', 'elit', 'consectetur', 'adipiscing', 'amet', 'blandit'],
   title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   type: 'Youtube',
+};
+
+const isFavorite: UserCardData = {
+  ...defaultData,
+  isFavorite: true,
+};
+
+const isToDo: UserCardData = {
+  ...defaultData,
+  isToDo: true,
+};
+
+const noTags: UserCardData = {
+  ...defaultData,
+  tags: [],
+};
+
+const someTags: UserCardData = {
+  ...defaultData,
+  tags: ['abc', 'd', 'e', 'f', 'xyz'],
+};
+
+export default {
+  defaultData,
+  isFavorite,
+  isToDo,
+  noTags,
+  someTags,
 };
